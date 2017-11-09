360a7dac3d453cdb67d63fcf64c579eb626dfeff
Robert Smith <smith.rob.s@gmail.com>
2017-11-09 12:01:13 -0500

    Support additional call endpoint types; closes #70

--- Nexmo.Api/Voice/Call.cs
+++ Nexmo.Api/Voice/Call.cs
@@ -9,9 +9,45 @@ namespace Nexmo.Api.Voice
     {
         public class Endpoint
         {
+            /// <summary>
+            /// One of the following: phone, websocket, sip
+            /// </summary>
             public string type { get; set; }
+
+            // phone:
+
+            /// <summary>
+            /// The phone number to connect to in E.164 format.
+            /// </summary>
             public string number { get; set; }
+            /// <summary>
+            /// Set the digits that are sent to the user as soon as the Call is answered. The * and # digits are respected. You create pauses using p. Each pause is 500ms.
+            /// </summary>
+            public string dtmfAnswer { get; set; }
+
+            // websocket/sip:
+
+            /// <summary>
+            /// The URI to the websocket you are streaming to.
+            /// OR
+            /// The SIP URI to the endpoint you are connecting to in the format sip:rebekka@sip.example.com.
+            /// </summary>
+            public string uri { get; set; }
+
+            // websocket:
+
+            /// <summary>
+            /// The internet media type for the audio you are streaming.Possible values are: audio/l16; rate=16000
+            /// </summary>
+            [JsonProperty("content-type")]
+            public string contentType { get; set; }
+
+            /// <summary>
+            /// A JSON object containing any metadata you want.
+            /// </summary>
+            public string headers { get; set; }
         }
+
         public class CallCommand
         {
             /// <summary>
