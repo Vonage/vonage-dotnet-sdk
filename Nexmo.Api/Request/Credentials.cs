d8aed10820d45a61c2de7bcfeabe7bff3c58a886
Robert Smith <smith.rob.s@gmail.com>
2018-02-21 08:13:47 -0500

    Document credentials fields

--- Nexmo.Api/Request/Credentials.cs
+++ Nexmo.Api/Request/Credentials.cs
@@ -2,13 +2,31 @@
 {
     public class Credentials
     {
+        /// <summary>
+        /// Nexmo API Key (from your account dashboard)
+        /// </summary>
         public string ApiKey { get; set; }
+        /// <summary>
+        /// Nexmo API Secret (from your account dashboard)
+        /// </summary>
         public string ApiSecret { get; set; }
+        /// <summary>
+        /// Signature Secret (from API settings section of your account settings)
+        /// </summary>
         public string SecuritySecret { get; set; }
+        /// <summary>
+        /// Application ID (GUID)
+        /// </summary>
         public string ApplicationId { get; set; }
+        /// <summary>
+        /// Application private key contents
+        /// This is the actual key file contents and NOT a path to the key file!
+        /// </summary>
         public string ApplicationKey { get; set; }
 
-        // Optional app useragent value to pass with every request
+        /// <summary>
+        /// (Optional) App useragent value to pass with every request
+        /// </summary>
         public string AppUserAgent { get; set; }
     }
 }
