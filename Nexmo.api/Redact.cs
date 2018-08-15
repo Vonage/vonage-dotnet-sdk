acff6e3f28aacebf5db89ac023e160f7d092334e
BiBi <rabeb.othmani@vonage.com>
2018-08-16 00:26:59 +0100

    updated README

--- Nexmo.api/Redact.cs
+++ Nexmo.api/Redact.cs
@@ -10,33 +10,49 @@ namespace Nexmo.Api
 {
     public static class Redact
     {
-        public class RedactCommand
+        public class RedactRequest
         {
             /// <summary>
             /// The transaction ID to redact
             /// </summary>
+            [JsonProperty("id")]
             public string Id { get; set; }
             /// <summary>
             /// Product name that the ID provided relates to
             /// Must be one of: sms, voice, number-insight, verify, verify-sdk, message or workflow
             /// </summary>
+            [JsonProperty("product")]
             public string Product { get; set; }
             /// <summary>
             /// Required if redacting SMS data
             /// Must be one of: inbound or outbound
             /// </summary>
+            [JsonProperty("type")]
             public string Type { get; set; }
+
+            public RedactRequest (string id, string product)
+            {
+                Id = id;
+                Product = product;
+            }
+            public RedactRequest(string id, string product, string type)
+            {
+                Id = id;
+                Product = product;
+                Type = type;
+            }
+
         }
 
         /// <summary>
         /// POST /v1/redact/transaction - redacts a specific transaction
         /// </summary>
-        /// <param name="cmd"></param>
+        /// <param name="redactRequest"></param>
         /// <param name="creds">(Optional) Overridden credentials for only this request</param>
         /// <returns></returns>
-        public static void RedactTransaction(RedactCommand cmd, Credentials creds = null)
+        public static void RedactTransaction(RedactRequest redactRequest, Credentials creds = null)
         {
-            VersionedApiRequest.DoRequest("POST", ApiRequest.GetBaseUriFor(typeof(Redact), "/v1/redact/transaction"), cmd, creds);
+            ApiRequest.MakePostRequest(ApiRequest.GetBaseUriFor(typeof(Redact), "/v1/redact/transaction"), redactRequest, creds);
         }
     }
 }
