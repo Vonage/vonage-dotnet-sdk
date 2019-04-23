6546e31afad4fc190cdc2ab442fee2de25654818
BiBi <rabeb.othmani@vonage.com>
2019-04-23 10:43:48 +0100

    WIP: getRecording

---
+++ Nexmo.Api/Voice/Recording.cs
@@ -0,0 +1,26 @@
+﻿using Nexmo.Api.Request;
+using System;
+using System.Net.Http;
+
+namespace Nexmo.Api.Voice
+{
+    public class Recording
+    {
+        public static object GetRecording(Uri recordingUrl, Credentials credentials = null)
+        {
+            if (recordingUrl == null)
+            {
+                throw new ArgumentNullException(nameof(recordingUrl));
+            }
+
+            var response = VersionedApiRequest.DoRequest("GET", recordingUrl), credentials);
+
+
+            return JsonConvert.DeserializeObject<CallResponse>(response.JsonResponse);
+
+            }
+
+            return null; 
+        }
+    }
+}
