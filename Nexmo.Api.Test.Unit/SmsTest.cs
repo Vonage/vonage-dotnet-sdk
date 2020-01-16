a3e75cb618b4943d80d38659c8e323c507f90421
slorello89 <slorello89@gmail.com>
2020-01-16 09:17:19 -0500

    committing unit test work

--- Nexmo.Api.Test.Unit/SmsTest.cs
+++ Nexmo.Api.Test.Unit/SmsTest.cs
@@ -236,5 +236,11 @@ namespace Nexmo.Api.UnitTest
             Assert.Equal(castObject.message_timestamp, System.DateTime.Parse("2020-01-01T12:00:00.000+00:00",ci));
 
         }
+
+        [Fact]
+        public void TestSendSmsWithSig()
+        {
+
+        }
     }
 }
