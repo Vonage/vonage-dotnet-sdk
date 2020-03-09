fd97e0a2f82060d2e6cef110b44c20af9350b84b
slorello89 <slorello89@gmail.com>
2020-03-09 15:27:30 -0400

    Adding factory methods for creating Credentials

--- Nexmo.Api/Request/Credentials.cs
+++ Nexmo.Api/Request/Credentials.cs
@@ -1,5 +1,5 @@
 ﻿using Nexmo.Api.Cryptography;
-
+using System.IO;
 namespace Nexmo.Api.Request
 {
     public class Credentials
@@ -55,5 +55,55 @@ namespace Nexmo.Api.Request
             ApplicationKey = nexmoApplicationPrivateKey;
         }
 
+        public static Credentials FromApiKeyAndSecret(string apiKey, string apiSecret)
+        {
+            return new Credentials(){ApiKey = apiKey, ApiSecret = apiSecret};
+        }
+
+        /// <summary>
+        /// Create Credentials from AppId and Private Key
+        /// </summary>
+        /// <param name="appId"></param>
+        /// <param name="privateKey"></param>
+        /// <returns></returns>
+        public static Credentials FromAppIdAndPrivateKey(string appId, string privateKey)
+        {
+            return new Credentials(){ApplicationId = appId, ApplicationKey = privateKey};
+        }
+
+        /// <summary>
+        /// Opens private key file and reads it
+        /// </summary>
+        /// <param name="appId">Your Nexmo Application ID</param>
+        /// <param name="privateKeyPath">path to your private Key file</param>
+        /// <exception cref="System.UnauthorizedAccessException">Thrown if app doesn't have requried permission to key file</exception>
+        /// <exception cref="System.ArgumentException">privateKeyPath is a zero length string, contains only white space or contains one or more invalid charecters</exception>
+        /// <exception cref="System.ArgumentNullException">privateKeyPath is null</exception>
+        /// <exception cref="System.IO.PathTooLongException">The specified path, filename, or both exceed the system-defined max length</exception>
+        /// <exception cref="System.IO.DirectoryNotFoundException">The specified path is invalid, (for example, it is on an unmapped drive).</exception>
+        /// <exception cref="System.IO.FileNotFoundException">The file specified in path was not found.</exception>
+        /// <exception cref="System.NotSupportedException">path is in an invalid format.</exception>
+        /// <returns>CredentialsObject</returns>
+        public static Credentials FromAppIdAndPrivateKeyPath(string appId, string privateKeyPath)
+        {
+            using (var reader = File.OpenText(privateKeyPath))
+            {
+                var privateKey = reader.ReadToEnd();
+                return new Credentials(){ApplicationId = appId, ApplicationKey = privateKey};
+            }
+        }
+        
+        /// <summary>
+        /// Builds Credentials from 
+        /// </summary>
+        /// <param name="apiKey"></param>
+        /// <param name="signatureSecret"></param>
+        /// <param name="method"></param>
+        /// <returns></returns>
+        public static Credentials FromApiKeySignatureSecretAndMethod(string apiKey, string signatureSecret,
+            SmsSignatureGenerator.Method method)
+        {
+            return new Credentials(){ApiKey = apiKey, SecuritySecret = signatureSecret, Method = method};
+        }
     }
 }
