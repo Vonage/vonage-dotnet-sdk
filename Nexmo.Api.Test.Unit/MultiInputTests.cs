e39127d73612d74c2254557746639daf338d7742
slorello89 <slorello89@gmail.com>
2020-06-02 17:12:05 -0400

    apparently first run at switching out ASR with multi-input tests didn't take

--- Nexmo.Api.Test.Unit/MultiInputTests.cs
+++ Nexmo.Api.Test.Unit/MultiInputTests.cs
@@ -11,40 +11,42 @@ using Newtonsoft.Json;
 namespace Nexmo.Api.Test.Unit
 {
     [TestClass]
-    public class SpeechAsrTests
+    public class MultiInputTests
     {
         [TestMethod]
         public void TestSerializeNccoKitchenSink()
         {
             // arrage
-            var expected = @"[{""speech"":{""uuid"":""aaaaaaaa-bbbb-cccc-dddd-0123456789ab"",""endOnSilence"":1,""language"":""en-US"",""context"":[""dog"",""cat""],""startTimeout"":5,""maxDuration"":30},""action"":""input""}]";
+            var expected = @"[{""dtmf"":{""timeOut"":3,""maxDigits"":1,""submitOnHash"":true},""speech"":{""uuid"":[""aaaaaaaa-bbbb-cccc-dddd-0123456789ab""],""endOnSilence"":1,""language"":""en-US"",""context"":[""dog"",""cat""],""startTimeout"":5,""maxDuration"":30},""action"":""input""}]";
             var settings = new SpeechSettings
             {
-                Uuid = "aaaaaaaa-bbbb-cccc-dddd-0123456789ab",
+                Uuid = new[] { "aaaaaaaa-bbbb-cccc-dddd-0123456789ab" },
                 EndOnSilence = 1,
                 Language = "en-US",
                 Context = new string[] { "dog", "cat" },
                 StartTimeout = 5,
                 MaxDuration = 30
             };
-            var inputAction = new InputAction { SpeechSettings = settings };
+            var dtmfSettings = new DtmfSettings { MaxDigits = 1, TimeOut = 3, SubmitOnHash = true };
+            var inputAction = new MultiInputAction { Speech = settings, Dtmf=dtmfSettings };
 
             //act
             var ncco = new Ncco(inputAction);
             var actual = ncco.ToString();
 
             //assert
-            Assert.AreEqual(expected, actual);
+             Assert.AreEqual(expected, actual);
         }
         [TestMethod]
         public void TestSerializeNccoKitchenEmpty()
         {
             // arrage
-            var expected = @"[{""speech"":{},""action"":""input""}]";
+            var expected = @"[{""dtmf"":{},""speech"":{""uuid"":[""aaaaaaaa-bbbb-cccc-dddd-0123456789ab""]},""action"":""input""}]";
             var settings = new SpeechSettings
-            {                
+            {
+                Uuid = new[] { "aaaaaaaa-bbbb-cccc-dddd-0123456789ab" }
             };
-            var inputAction = new InputAction { SpeechSettings = settings };
+            var inputAction = new MultiInputAction { Speech = settings };
 
             //act
             var ncco = new Ncco(inputAction);
@@ -76,19 +78,25 @@ namespace Nexmo.Api.Test.Unit
                   }
                 ]
               },              
+              ""dtmf"": {
+                ""digits"": ""1234"",
+                ""timed_out"": false
+              },
               ""uuid"": ""aaaaaaaa-bbbb-cccc-dddd-0123456789ab"",
               ""conversation_uuid"": ""bbbbbbbb-cccc-dddd-eeee-0123456789ab"",
               ""timestamp"": ""2020-01-01T14:00:00.000Z""
             }";
 
-            var serialized = JsonConvert.DeserializeObject<Input>(inboundString);
+            var serialized = JsonConvert.DeserializeObject<MultiInput>(inboundString);
 
             Assert.AreEqual("aaaaaaaa-bbbb-cccc-dddd-0123456789ab", serialized.Uuid);            
-            Assert.AreEqual("end_on_silence_timeout", serialized.SpeechResult.TimeoutReason);
-            Assert.AreEqual("0.9405097", serialized.SpeechResult.SpeechResults[0].Confidence);
-            Assert.AreEqual("Sales", serialized.SpeechResult.SpeechResults[0].Text);
-            Assert.AreEqual("0.5949854", serialized.SpeechResult.SpeechResults[2].Confidence);
-            Assert.AreEqual("Sale", serialized.SpeechResult.SpeechResults[2].Text);
+            Assert.AreEqual("end_on_silence_timeout", serialized.Speech.TimeoutReason);
+            Assert.AreEqual("0.9405097", serialized.Speech.SpeechResults[0].Confidence);
+            Assert.AreEqual("Sales", serialized.Speech.SpeechResults[0].Text);
+            Assert.AreEqual("0.5949854", serialized.Speech.SpeechResults[2].Confidence);
+            Assert.AreEqual("Sale", serialized.Speech.SpeechResults[2].Text);
+            Assert.AreEqual("1234", serialized.Dtmf.Digits);
+            Assert.AreEqual(false, serialized.Dtmf.TimedOut);
         }
     }
 }
