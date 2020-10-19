d2e712963371a26880dc51ca12abc8e6de143beb
ParamGabhawala <70215037+ParamGabhawala@users.noreply.github.com>
2020-10-20 00:13:20 +0530

    Update NexmoException.cs

--- Vonage/NexmoException.cs
+++ Vonage/NexmoException.cs
@@ -11,3 +11,86 @@ namespace Vonage
         public VonageException(string message) : base(message) { }
     }
 }
+using System;
+using Vonage.Request;
+
+namespace Vonage.Accounts
+{
+    public class AccountClient : IAccountClient
+    {
+        public Credentials Credentials { get; set; }
+        public AccountClient(Credentials creds = null)
+        {
+            Credentials = creds;
+        }
+
+        public Balance GetAccountBalance(Credentials creds = null)
+        {
+            return ApiRequest.DoGetRequestWithQueryParameters<Balance>(
+                ApiRequest.GetBaseUriFor(typeof(AccountClient),
+                "/account/get-balance"), 
+                ApiRequest.AuthType.Query, 
+                credentials: creds ?? Credentials);
+        }
+
+        public TopUpResult TopUpAccountBalance(TopUpRequest request, Credentials creds = null)
+        {
+            return ApiRequest.DoGetRequestWithQueryParameters<TopUpResult>(
+                ApiRequest.GetBaseUriFor(typeof(AccountClient), "/account/top-up"),
+                ApiRequest.AuthType.Query,
+                request,
+                credentials:creds ?? Credentials
+            );
+        }
+
+        public AccountSettingsResult ChangeAccountSettings(AccountSettingsRequest request, Credentials creds = null)
+        {
+            return ApiRequest.DoPostRequestUrlContentFromObject<AccountSettingsResult>
+            (
+                ApiRequest.GetBaseUriFor(typeof(AccountClient), "/account/settings"),
+                request,
+                creds ?? Credentials
+            );
+        }
+
+        public SecretsRequestResult RetrieveApiSecrets(string apiKey, Credentials creds = null)
+        {
+            return ApiRequest.DoGetRequestWithQueryParameters<SecretsRequestResult>(
+                ApiRequest.GetBaseUri(ApiRequest.UriType.Api, $"/accounts/{apiKey}/secrets"),
+                ApiRequest.AuthType.Basic,
+                credentials: creds ?? Credentials
+            );
+        }
+
+        public Secret CreateApiSecret(CreateSecretRequest request, string apiKey, Credentials creds = null)
+        {
+            return ApiRequest.DoRequestWithJsonContent<Secret>(
+                "POST",
+                ApiRequest.GetBaseUri(ApiRequest.UriType.Api, $"/accounts/{apiKey}/secrets"),
+                request,
+                ApiRequest.AuthType.Basic,
+                creds: creds ?? Credentials
+            );
+        }
+
+        public Secret RetrieveApiSecret(string secretId, string apiKey, Credentials creds = null)
+        {
+            return ApiRequest.DoGetRequestWithQueryParameters<Secret>(
+                ApiRequest.GetBaseUri(ApiRequest.UriType.Api, $"/accounts/{apiKey}/secrets/{secretId}"),
+                ApiRequest.AuthType.Basic,
+                credentials: creds ?? Credentials
+            );
+        }
+
+        public bool RevokeApiSecret(string secretId, string apiKey, Credentials creds = null)
+        {
+            ApiRequest.DoDeleteRequestWithUrlContent(
+                ApiRequest.GetBaseUri(ApiRequest.UriType.Api, $"/accounts/{apiKey}/secrets/{secretId}"),
+                null,
+                ApiRequest.AuthType.Basic,
+                creds ?? Credentials
+            );
+            return true;
+        }        
+    }
+}
