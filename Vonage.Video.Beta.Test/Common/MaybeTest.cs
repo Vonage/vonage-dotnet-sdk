fe7a320746eaf1fbc134ae90dd1adf98c21bdd02
Tr00d <guillaume.faas@live.fr>
2022-12-09 07:46:28 +0100

    Add more client frameworks on test project, remove global usings

--- Vonage.Video.Beta.Test/Common/MaybeTest.cs
+++ Vonage.Video.Beta.Test/Common/MaybeTest.cs
@@ -1,64 +1,67 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
 using Vonage.Video.Beta.Common;
+using Xunit;
 
-namespace Vonage.Video.Beta.Test.Common;
-
-public class MaybeTest
+namespace Vonage.Video.Beta.Test.Common
 {
-    [Fact]
-    public void None_ShouldReturnNone()
+    public class MaybeTest
     {
-        var maybe = Maybe<int>.None;
-        maybe.IsNone.Should().BeTrue();
-        maybe.IsSome.Should().BeFalse();
+        [Fact]
+        public void None_ShouldReturnNone()
+        {
+            var maybe = Maybe<int>.None;
+            maybe.IsNone.Should().BeTrue();
+            maybe.IsSome.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Some_ShouldReturnSome()
+        {
+            var maybe = CreateSome(10);
+            maybe.IsNone.Should().BeFalse();
+            maybe.IsSome.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Map_ShouldReturnNone_GivenValueIsNone() =>
+            Maybe<int>.None
+                .Map(MapToString)
+                .Should()
+                .Be(Maybe<string>.None);
+
+        [Fact]
+        public void Map_ShouldReturnSome_GivenValueIsSome() =>
+            CreateSome(10)
+                .Map(MapToString)
+                .Should()
+                .Be(CreateSome("10"));
+
+        [Fact]
+        public void Match_ShouldReturnNoneOperation_GivenValueIsNone() =>
+            Maybe<int>.None
+                .Match(MapToString, GetStaticString)
+                .Should()
+                .Be("Some value");
+
+        [Fact]
+        public void Match_ShouldReturnSomeOperation_GivenValueIsSome() =>
+            CreateSome(10)
+                .Match(MapToString, GetStaticString)
+                .Should()
+                .Be("10");
+
+        [Fact]
+        public void Some_ShouldThrowException_GivenValueIsNone()
+        {
+            Action act = () => Maybe<int?>.Some<int?>(null);
+            act.Should().Throw<InvalidOperationException>().WithMessage(Maybe<int>.NullValueMessage);
+        }
+
+        private static Maybe<T> CreateSome<T>(T value) => Maybe<T>.Some(value);
+
+        private static string GetStaticString() => "Some value";
+
+        private static string MapToString(int value) => value.ToString();
     }
-
-    [Fact]
-    public void Some_ShouldReturnSome()
-    {
-        var maybe = CreateSome(10);
-        maybe.IsNone.Should().BeFalse();
-        maybe.IsSome.Should().BeTrue();
-    }
-
-    [Fact]
-    public void Map_ShouldReturnNone_GivenValueIsNone() =>
-        Maybe<int>.None
-            .Map(MapToString)
-            .Should()
-            .Be(Maybe<string>.None);
-
-    [Fact]
-    public void Map_ShouldReturnSome_GivenValueIsSome() =>
-        CreateSome(10)
-            .Map(MapToString)
-            .Should()
-            .Be(CreateSome("10"));
-
-    [Fact]
-    public void Match_ShouldReturnNoneOperation_GivenValueIsNone() =>
-        Maybe<int>.None
-            .Match(MapToString, GetStaticString)
-            .Should()
-            .Be("Some value");
-
-    [Fact]
-    public void Match_ShouldReturnSomeOperation_GivenValueIsSome() =>
-        CreateSome(10)
-            .Match(MapToString, GetStaticString)
-            .Should()
-            .Be("10");
-
-    [Fact]
-    public void Some_ShouldThrowException_GivenValueIsNone()
-    {
-        Action act = () => Maybe<int?>.Some<int?>(null);
-        act.Should().Throw<InvalidOperationException>().WithMessage(Maybe<int>.NullValueMessage);
-    }
-
-    private static Maybe<T> CreateSome<T>(T value) => Maybe<T>.Some(value);
-
-    private static string GetStaticString() => "Some value";
-
-    private static string MapToString(int value) => value.ToString();
 }
--- Vonage.Video.Beta.Test/Common/ResultTest.cs
+++ Vonage.Video.Beta.Test/Common/ResultTest.cs
@@ -1,57 +1,59 @@
 ﻿using FluentAssertions;
 using Vonage.Video.Beta.Common;
+using Xunit;
 
-namespace Vonage.Video.Beta.Test.Common;
-
-public class ResultTest
+namespace Vonage.Video.Beta.Test.Common
 {
-    [Fact]
-    public void FromError_ShouldReturnError()
+    public class ResultTest
     {
-        var result = CreateFailure();
-        result.IsFailure.Should().BeTrue();
-        result.IsSuccess.Should().BeFalse();
+        [Fact]
+        public void FromError_ShouldReturnError()
+        {
+            var result = CreateFailure();
+            result.IsFailure.Should().BeTrue();
+            result.IsSuccess.Should().BeFalse();
+        }
+
+        [Fact]
+        public void FromSuccess_ShouldReturnSuccess()
+        {
+            var result = CreateSuccess(0);
+            result.IsFailure.Should().BeFalse();
+            result.IsSuccess.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Map_ShouldReturnFailure_GivenValueIsFailure() =>
+            CreateFailure()
+                .Map(Increment)
+                .Should()
+                .Be(CreateFailure());
+
+        [Fact]
+        public void Map_ShouldReturnSuccess_GivenValueIsSuccess() =>
+            CreateSuccess(5)
+                .Map(Increment)
+                .Should()
+                .Be(CreateSuccess(6));
+
+        [Fact]
+        public void Match_ShouldReturnFailureOperation_GivenValueIsFailure() =>
+            CreateFailure()
+                .Match(value => value.ToString(), failure => failure.Error)
+                .Should()
+                .Be("Some error");
+
+        [Fact]
+        public void Match_ShouldReturnSuccessOperation_GivenValueIsSuccess() =>
+            CreateSuccess(5)
+                .Match(Increment, _ => 0)
+                .Should()
+                .Be(6);
+
+        private static Result<int> CreateSuccess(int value) => Result<int>.FromSuccess(value);
+
+        private static Result<int> CreateFailure() => Result<int>.FromFailure(new ResultFailure("Some error"));
+
+        private static int Increment(int value) => value + 1;
     }
-
-    [Fact]
-    public void FromSuccess_ShouldReturnSuccess()
-    {
-        var result = CreateSuccess(0);
-        result.IsFailure.Should().BeFalse();
-        result.IsSuccess.Should().BeTrue();
-    }
-
-    [Fact]
-    public void Map_ShouldReturnFailure_GivenValueIsFailure() =>
-        CreateFailure()
-            .Map(Increment)
-            .Should()
-            .Be(CreateFailure());
-
-    [Fact]
-    public void Map_ShouldReturnSuccess_GivenValueIsSuccess() =>
-        CreateSuccess(5)
-            .Map(Increment)
-            .Should()
-            .Be(CreateSuccess(6));
-
-    [Fact]
-    public void Match_ShouldReturnFailureOperation_GivenValueIsFailure() =>
-        CreateFailure()
-            .Match(value => value.ToString(), failure => failure.Error)
-            .Should()
-            .Be("Some error");
-
-    [Fact]
-    public void Match_ShouldReturnSuccessOperation_GivenValueIsSuccess() =>
-        CreateSuccess(5)
-            .Match(Increment, _ => 0)
-            .Should()
-            .Be(6);
-
-    private static Result<int> CreateSuccess(int value) => Result<int>.FromSuccess(value);
-
-    private static Result<int> CreateFailure() => Result<int>.FromFailure(new ResultFailure("Some error"));
-
-    private static int Increment(int value) => value + 1;
 }
