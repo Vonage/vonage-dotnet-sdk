1b60dd04a6a18e319a773f765da90da0f550afd1
Tr00d <guillaume.faas@live.fr>
2023-01-03 14:28:52 +0100

    Implement deserialization tests for archiving

--- Vonage.Video.Beta.Test/Common/Failures/HttpFailureDeserializationTest.cs
+++ Vonage.Video.Beta.Test/Common/Failures/HttpFailureDeserializationTest.cs
@@ -18,6 +18,7 @@ namespace Vonage.Video.Beta.Test.Common.Failures
         [InlineData("403", HttpStatusCode.Forbidden, "You passed in an invalid Vonage JWT token.")]
         [InlineData("404", HttpStatusCode.NotFound, "The session exists but has not had any streams added to it yet.")]
         [InlineData("408", HttpStatusCode.RequestTimeout, "You passed in an invalid stream ID.")]
+        [InlineData("409", HttpStatusCode.Conflict, "You attempted to stop an archive that was not being recorded")]
         [InlineData("413", HttpStatusCode.RequestEntityTooLarge,
             "The type string exceeds the maximum length (128 bytes), or the data string exceeds the maximum size (8 kB).")]
         public void ShouldDeserializeError(string code, HttpStatusCode statusCode, string message) =>
