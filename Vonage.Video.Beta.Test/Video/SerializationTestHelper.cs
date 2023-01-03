b3fc17e0e2a2fbda4c2eb06beb3ee7d77c8dac3e
Tr00d <guillaume.faas@live.fr>
2023-01-03 11:09:27 +0100

    Refactor serialization tests

---
+++ Vonage.Video.Beta.Test/Video/SerializationTestHelper.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using Vonage.Video.Beta.Common;
+
+namespace Vonage.Video.Beta.Test.Video
+{
+    public class SerializationTestHelper
+    {
+        private const string ExcludeNamespace = "Vonage.Video.Beta.Test.";
+        private readonly string callerNamespace;
+
+        public JsonSerializer Serializer { get; }
+
+        public SerializationTestHelper(string callerNamespace)
+        {
+            this.callerNamespace = callerNamespace;
+            this.Serializer = new JsonSerializer();
+        }
+
+        public string GetResponseJson([CallerMemberName] string name = null)
+        {
+            var filePath = string.Concat(this.GetUseCaseFolder(), GetRelativeFilePath(name));
+            return File.Exists(filePath)
+                ? CleanJsonContent(File.ReadAllText(filePath))
+                : string.Empty;
+        }
+
+        private static string CleanJsonContent(string filePath) =>
+            Regex.Replace(filePath, "(\"(?:[^\"\\\\]|\\\\.)*\")|\\s+", "$1");
+
+        private string GetUseCaseFolder() =>
+            this.callerNamespace
+                .Replace(ExcludeNamespace, string.Empty)
+                .Replace('.', '/');
+
+        private static string GetRelativeFilePath(string caller) => $"/Data/{caller}-response.json";
+    }
+}
--- Vonage.Video.Beta.Test/Video/Sessions/CreateSession/CreateSessionDeserializationTest.cs
+++ Vonage.Video.Beta.Test/Video/Sessions/CreateSession/CreateSessionDeserializationTest.cs
@@ -1,8 +1,5 @@
-﻿using System.IO;
-using System.Runtime.CompilerServices;
-using System.Text.RegularExpressions;
+﻿using System;
 using FluentAssertions;
-using Vonage.Video.Beta.Common;
 using Vonage.Video.Beta.Video.Sessions.CreateSession;
 using Xunit;
 
@@ -10,42 +7,31 @@ namespace Vonage.Video.Beta.Test.Video.Sessions.CreateSession
 {
     public class CreateSessionDeserializationTest
     {
-        private readonly JsonSerializer serializer;
+        private readonly SerializationTestHelper helper;
 
         public CreateSessionDeserializationTest()
         {
-            this.serializer = new JsonSerializer();
+            this.helper = new SerializationTestHelper(typeof(CreateSessionDeserializationTest).Namespace);
         }
 
         [Fact]
         public void ShouldDeserialize()
         {
-            var response = this.serializer.DeserializeObject<CreateSessionResponse[]>(this.GetResponseJson());
-            response.IsSuccess.Should().BeTrue();
-            var content = response.GetSuccessUnsafe();
+            const string expectedId =
+                "2_MX5hOThlMTJjYS1mM2U1LTRkZjgtYmM2Ni1mZDRiNWYzMGI5ZTl-fjE2NzI3MzY4NzgxNjJ-bi9OeFVLbkNaVjBUUnpVSmxjbURqQ3J4flB-fg";
+            var response =
+                this.helper.Serializer.DeserializeObject<CreateSessionResponse[]>(this.helper.GetResponseJson());
+            var content = response.Match(_ => _, _ => throw new InvalidOperationException());
             content.Length.Should().Be(1);
-            content[0].SessionId.Should()
-                .Be(
-                    "2_MX5hOThlMTJjYS1mM2U1LTRkZjgtYmM2Ni1mZDRiNWYzMGI5ZTl-fjE2NzI3MzY4NzgxNjJ-bi9OeFVLbkNaVjBUUnpVSmxjbURqQ3J4flB-fg");
+            content[0].SessionId.Should().Be(expectedId);
         }
 
         [Fact]
         public void ShouldDeserialize_GivenEmptyArray()
         {
-            var response = this.serializer.DeserializeObject<CreateSessionResponse[]>(this.GetResponseJson());
-            response.IsSuccess.Should().BeTrue();
-            response.GetSuccessUnsafe().Should().BeEmpty();
-        }
-
-        private string GetResponseJson([CallerMemberName] string name = null)
-        {
-            var folder = this.GetType().Namespace?
-                .Replace("Vonage.Video.Beta.Test.", string.Empty)
-                .Replace('.', '/');
-            var test = $"{folder}/Data/{name}-response.json";
-            return File.Exists(test)
-                ? Regex.Replace(File.ReadAllText(test), "(\"(?:[^\"\\\\]|\\\\.)*\")|\\s+", "$1")
-                : string.Empty;
+            var response =
+                this.helper.Serializer.DeserializeObject<CreateSessionResponse[]>(this.helper.GetResponseJson());
+            response.Match(_ => _, _ => throw new InvalidOperationException()).Should().BeEmpty();
         }
     }
 }
