f7257c68d1afe58f48e9ebec546f6b24bc9b000d
Tr00d <guillaume.faas@live.fr>
2023-01-11 13:32:17 +0100

    Add classes DeliveryReceipt and InboundMessage for WhatsApp

---
+++ Vonage/Messages/WhatsApp/WhatsAppDeliveryReceipt.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Vonage.Messages.WhatsApp;
+
+public class WhatsAppDeliveryReceipt
+{
+    [JsonProperty("channel")] public MessagesChannel Channel { get; set; }
+
+    [JsonProperty("client_ref")] public string ClientRef { get; set; }
+
+    [JsonProperty("error")] public Error Error { get; set; }
+
+    [JsonProperty("from")] public string From { get; set; }
+
+    [JsonProperty("message_uuid")] public string MessageUuid { get; set; }
+
+    [JsonProperty("status")] public MessagesDlrStatus Status { get; set; }
+
+    [JsonProperty("timestamp")] public DateTime Timestamp { get; set; }
+
+    [JsonProperty("to")] public string To { get; set; }
+
+    [JsonProperty("usage")] public Usage Usage { get; set; }
+
+    [JsonProperty("whatsapp")] public WhatsApp WhatsApp { get; set; }
+}
+
+public enum MessagesDlrStatus
+{
+    Submitted,
+    Delivered,
+    Rejected,
+    Undeliverable,
+    Read,
+}
+
+public class Error
+{
+    [JsonProperty("detail")] public string Detail { get; set; }
+
+    [JsonProperty("instance")] public string Instance { get; set; }
+
+    [JsonProperty("title")] public string Title { get; set; }
+
+    [JsonProperty("type")] public string Type { get; set; }
+}
+
+public class Conversation
+{
+    [JsonProperty("id")] public string Id { get; set; }
+
+    [JsonProperty("origin")] public Origin Origin { get; set; }
+}
+
+public class Origin
+{
+    [JsonProperty("type")] public string Type { get; set; }
+}
+
+public class Usage
+{
+    [JsonProperty("currency")] public string Currency { get; set; }
+
+    [JsonProperty("price")] public string Price { get; set; }
+}
+
+public class WhatsApp
+{
+    [JsonProperty("conversation")] public Conversation Conversation { get; set; }
+}
---
+++ Vonage/Messages/WhatsApp/WhatsAppInboundMessage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Vonage.Messages.WhatsApp;
+
+public class WhatsAppInboundMessage
+{
+    [JsonProperty("channel")] public MessagesChannel Channel { get; set; }
+
+    [JsonProperty("client_ref")] public string ClientRef { get; set; }
+
+    [JsonProperty("context")] public Context Context { get; set; }
+
+    [JsonProperty("from")] public string From { get; set; }
+
+    [JsonProperty("message_type")] public MessagesMessageType MessageType { get; set; }
+
+    [JsonProperty("message_uuid")] public string MessageUuid { get; set; }
+
+    [JsonProperty("profile")] public Profile Profile { get; set; }
+
+    [JsonProperty("provider_message")] public string ProviderMessage { get; set; }
+
+    [JsonProperty("text")] public string Text { get; set; }
+
+    [JsonProperty("timestamp")] public DateTime Timestamp { get; set; }
+
+    [JsonProperty("to")] public string To { get; set; }
+}
+
+public class Profile
+{
+    [JsonProperty("name")] public string Name { get; set; }
+}
+
+public class Context
+{
+    [JsonProperty("message_from")] public string MessageFrom { get; set; }
+
+    [JsonProperty("message_uuid")] public string MessageUuid { get; set; }
+
+    [JsonProperty("whatsapp_referred_product")]
+    public WhatsAppReferredProduct WhatsAppReferredProduct { get; set; }
+}
+
+public class WhatsAppReferredProduct
+{
+    [JsonProperty("catalog_id")] public string CatalogId { get; set; }
+
+    [JsonProperty("product_retailer_id")] public string ProductRetailerId { get; set; }
+}
