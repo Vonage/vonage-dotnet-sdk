03cc7476c922222710895b774a9e029f40183907
Tr00d <guillaume.faas@live.fr>
2023-01-20 14:50:48 +0100

    Implement GetDialNumbersRequest

---
+++ Vonage/Meetings/GetDialNumbers/GetDialNumbersRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using Vonage.Common.Client;
+
+namespace Vonage.Meetings.GetDialNumbers;
+
+/// <summary>
+///     Represents a request to retrieve dial-in numbers.
+/// </summary>
+public readonly struct GetDialNumbersRequest : IVonageRequest
+{
+    /// <inheritdoc />
+    public HttpRequestMessage BuildRequestMessage(string token)
+    {
+        var httpRequest = new HttpRequestMessage(HttpMethod.Get, this.GetEndpointPath());
+        httpRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        return httpRequest;
+    }
+
+    /// <inheritdoc />
+    public string GetEndpointPath() => "/beta/meetings/dial-in-numbers";
+}
