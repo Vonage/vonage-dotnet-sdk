e08e1cbba1141b7ae4667289733ad19d1cf57dc0
Tr00d <guillaume.faas@live.fr>
2023-02-03 15:40:47 +0100

    Add missing Xml Doc tags

--- Vonage/Meetings/Common/ThemeLogoType.cs
+++ Vonage/Meetings/Common/ThemeLogoType.cs
@@ -2,9 +2,19 @@
 
 namespace Vonage.Meetings.Common;
 
+/// <summary>
+/// </summary>
 public enum ThemeLogoType
 {
+    /// <summary>
+    /// </summary>
     [Description("white")] White,
+
+    /// <summary>
+    /// </summary>
     [Description("colored")] Colored,
+
+    /// <summary>
+    /// </summary>
     [Description("favicon")] Favicon,
 }
