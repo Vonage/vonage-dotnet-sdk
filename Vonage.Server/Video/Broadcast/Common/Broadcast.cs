fc69b246f31bbccb0168d8ecf302c8a0ab11397c
Tr00d <guillaume.faas@live.fr>
2023-02-20 14:14:51 +0100

    Fill Xml Documentation on Broadcast

--- Vonage.Server/Video/Broadcast/Common/Broadcast.cs
+++ Vonage.Server/Video/Broadcast/Common/Broadcast.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Vonage.Server.Video.Broadcast.Common;
 
@@ -8,53 +7,179 @@ namespace Vonage.Server.Video.Broadcast.Common;
 /// </summary>
 public struct Broadcast
 {
+    /// <summary>
+    ///     The Vonage Application UUID.
+    /// </summary>
     public Guid ApplicationId { get; set; }
+
+    /// <summary>
+    ///     An object containing details about the HLS and RTMP broadcasts.
+    /// </summary>
     public BroadcastUrl BroadcastUrls { get; set; }
+
+    /// <summary>
+    ///     The time the broadcast started, expressed in milliseconds since the Unix epoch (January 1, 1970, 00:00:00 UTC).
+    /// </summary>
     public long CreatedAt { get; set; }
+
+    /// <summary>
+    /// </summary>
     public bool HasAudio { get; set; }
+
+    /// <summary>
+    /// </summary>
     public bool HasVideo { get; set; }
+
+    /// <summary>
+    ///     The unique ID for the broadcast.
+    /// </summary>
     public string Id { get; set; }
+
+    /// <summary>
+    /// </summary>
     public int MaxBitrate { get; set; }
+
+    /// <summary>
+    ///     The maximum duration for the broadcast (if one was set), in seconds.
+    /// </summary>
     public long MaxDuration { get; set; }
+
+    /// <summary>
+    ///     The unique tag for simultaneous broadcasts (if one was set).
+    /// </summary>
     public string MultiBroadcastTag { get; set; }
+
+    /// <summary>
+    ///     The resolution of the archive, either "640x480" (SD landscape, the default), "1280x720" (HD landscape), "1920x1080"
+    ///     (FHD landscape), "480x640" (SD portrait), "720x1280" (HD portrait),or "1080x1920" (FHD portrait). You may want to
+    ///     use a portrait aspect ratio for archives that include video streams from mobile devices (which often use the
+    ///     portrait aspect ratio). This property only applies to composed archives. If you set this property and set the
+    ///     outputMode property to "individual", the call to the REST method results in an error.
+    /// </summary>
     public string Resolution { get; set; }
+
+    /// <summary>
+    ///     The OpenTok session ID.
+    /// </summary>
     public string SessionId { get; set; }
+
+    /// <summary>
+    /// </summary>
     public BroadcastSettings Settings { get; set; }
+
+    /// <summary>
+    ///     Current status of the broadcast.
+    /// </summary>
     public string Status { get; set; }
+
+    /// <summary>
+    ///     Whether streams included in the archive are selected automatically ("auto", the default) or manually ("manual").
+    ///     When streams are selected automatically ("auto"), all streams in the session can be included in the archive. When
+    ///     streams are selected manually ("manual"), you specify streams to be included based on calls to this REST method.
+    ///     You can specify whether a stream's audio, video, or both are included in the archive. In composed archives, in both
+    ///     automatic and manual modes, the archive composer includes streams based on stream prioritization rules.
+    /// </summary>
     public string StreamMode { get; set; }
-    public List<BroadcastStream> Streams { get; set; }
+
+    /// <summary>
+    ///     An array of objects corresponding to streams currently being broadcast. This is only set for a broadcast with the
+    ///     status set to "started" and the streamMode set to "manual"
+    /// </summary>
+    public LiveStream[] Streams { get; set; }
+
+    /// <summary>
+    ///     For this start method, this timestamp matches the createdAt timestamp.
+    /// </summary>
     public long UpdatedAt { get; set; }
 
+    /// <summary>
+    ///     Represents information regarding HLS and RTMP broadcasts.
+    /// </summary>
     public struct BroadcastUrl
     {
+        /// <summary>
+        ///     If you specified an HLS endpoint, the object includes an hls property, which is set to the URL for the HLS
+        ///     broadcast. Note this HLS broadcast URL points to an index file, an .M3U8- formatted playlist that contains a list
+        ///     of URLs to .ts media segment files (MPEG-2 transport stream files). While the URLs of both the playlist index file
+        ///     and media segment files are provided as soon as the HTTP response is returned, these URLs should not be accessed
+        ///     until 15 – 20 seconds later, after the initiation of the HLS broadcast, due to the delay between the HLS broadcast
+        ///     and the live streams in the OpenTok session. See
+        ///     https://developer.apple.com/library/ios/technotes/tn2288/_index.html for more information about the playlist index
+        ///     file and media segment files for HLS.
+        /// </summary>
         public Uri Hls { get; set; }
 
-        public List<BroadcastUrlRtmp> Rtmp { get; set; }
+        /// <summary>
+        ///     If you specified RTMP stream endpoints, the object includes an rtmp property. This is an array of objects that
+        ///     include information on each of the RTMP streams. Each of these objects has the following properties: id (the ID you
+        ///     assigned to the RTMP stream),serverUrl (the server URL), streamName (the stream name), and status property (which
+        ///     is set to "connecting"). You can call the OpenTok REST method to check for status updates for the broadcast.
+        /// </summary>
+        public RtmpStream[] Rtmp { get; set; }
 
-        public struct BroadcastUrlRtmp
+        /// <summary>
+        ///     Represents a RTMP stream.
+        /// </summary>
+        public struct RtmpStream
         {
+            /// <summary>
+            /// </summary>
             public string Id { get; set; }
+
+            /// <summary>
+            /// </summary>
             public string ServerUrl { get; set; }
+
+            /// <summary>
+            /// </summary>
             public string Status { get; set; }
+
+            /// <summary>
+            /// </summary>
             public string StreamName { get; set; }
         }
     }
 
+    /// <summary>
+    /// </summary>
     public struct BroadcastSettings
     {
+        /// <summary>
+        /// </summary>
         public BroadcastSettingsHls Hls { get; set; }
 
+        /// <summary>
+        /// </summary>
         public struct BroadcastSettingsHls
         {
+            /// <summary>
+            /// </summary>
             public bool Dvr { get; set; }
+
+            /// <summary>
+            /// </summary>
             public bool LowLatency { get; set; }
         }
     }
 
-    public struct BroadcastStream
+    /// <summary>
+    ///     Represents a stream currently being broadcast.
+    /// </summary>
+    public struct LiveStream
     {
+        /// <summary>
+        ///     Whether the stream's audio is included in the broadcast.
+        /// </summary>
         public bool HasAudio { get; set; }
+
+        /// <summary>
+        ///     Whether the stream's video is included in the broadcast.
+        /// </summary>
         public bool HasVideo { get; set; }
+
+        /// <summary>
+        ///     The stream ID of the stream included in the broadcast.
+        /// </summary>
         public string StreamId { get; set; }
     }
 }
