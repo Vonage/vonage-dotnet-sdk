27115935f49d9157b327b0ea22b0935b6ddcaa44
tr00d <guillaume.faas@live.fr>
2023-03-06 10:12:11 +0100

    Use factory method to create AuthenticationException based on scenarios

--- Vonage.Common/Exceptions/VonageAuthenticationException.cs
+++ Vonage.Common/Exceptions/VonageAuthenticationException.cs
@@ -2,10 +2,27 @@
 
 namespace Vonage.Common.Exceptions
 {
+    /// <summary>
+    ///     Represents an issue related to authentication.
+    /// </summary>
     public class VonageAuthenticationException : Exception
     {
-        public VonageAuthenticationException(string message) : base(message)
+        private VonageAuthenticationException(string message) : base(message)
         {
         }
+
+        /// <summary>
+        ///     Creates an exception indicating the ApiKey or ApiSecret are missing.
+        /// </summary>
+        /// <returns>The exception.</returns>
+        public static VonageAuthenticationException FromMissingApiKeyOrSecret() =>
+            new("API Key or API Secret missing.");
+
+        /// <summary>
+        ///     Creates an exception indicating the ApplicationId or PrivateKeyPath are missing.
+        /// </summary>
+        /// <returns>The exception.</returns>
+        public static VonageAuthenticationException FromMissingApplicationIdOrPrivateKeyPath() =>
+            new("AppId or Private Key Path missing.");
     }
 }
