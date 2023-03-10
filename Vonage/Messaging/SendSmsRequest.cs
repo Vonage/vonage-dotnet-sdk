ebf83cfb494c5a22a52729428d3b3af62bcdd68d
tr00d <guillaume.faas@live.fr>
2023-03-10 14:47:17 +0100

    Remove additional wappush, vcal and vcard properties

--- Vonage/Messaging/SendSmsRequest.cs
+++ Vonage/Messaging/SendSmsRequest.cs
@@ -6,6 +6,46 @@ namespace Vonage.Messaging
     public class SendSmsRequest
     {
         /// <summary>
+        /// An optional string used to identify separate accounts using the SMS endpoint for billing purposes. 
+        /// To use this feature, please email support@nexmo.com
+        /// </summary>
+        [JsonProperty("account-ref")]
+        public string AccountRef { get; set; }
+
+        /// <summary>
+        /// Hex encoded binary data. Depends on type parameter having the value binary.
+        /// </summary>
+        [JsonProperty("body")]
+        public string Body { get; set; }
+
+        /// <summary>
+        /// The webhook endpoint the delivery receipt for this sms is sent to. 
+        /// This parameter overrides the webhook endpoint you set in Dashboard.
+        /// </summary>
+        [JsonProperty("callback")]
+        public string Callback { get; set; }
+
+        /// <summary>
+        /// You can optionally include your own reference of up to 40 characters.
+        /// </summary>
+        [JsonProperty("client-ref")]
+        public string ClientRef { get; set; }
+
+        /// <summary>
+        /// A string parameter that satisfies regulatory requirements when sending an SMS to specific countries.
+        /// For more information please refer to the <see href="https://help.nexmo.com/hc/en-us/articles/115011781468">Country-Specific Outbound SMS Features</see>
+        /// </summary>
+        [JsonProperty("content-id")]
+        public string ContentId { get; set; }
+
+        /// <summary>
+        /// A string parameter that satisfies regulatory requirements when sending an SMS to specific countries.
+        /// For more information please refer to the <see href="https://help.nexmo.com/hc/en-us/articles/115011781468">Country-Specific Outbound SMS Features</see>
+        /// </summary>
+        [JsonProperty("entity-id")]
+        public string EntityId { get; set; }
+
+        /// <summary>
         /// The name or number the message should be sent from. 
         /// Alphanumeric senderID's are not supported in all countries, 
         /// see Global Messaging for more details. If alphanumeric, 
@@ -15,10 +55,23 @@ namespace Vonage.Messaging
         public string From { get; set; }
 
         /// <summary>
-        /// The number that the message should be sent to. Numbers are specified in E.164 format.
+        ///  The Data Coding Scheme value of the message
+        ///  Must be one of: 0, 1, 2 or 3
         /// </summary>
-        [JsonProperty("to")]
-        public string To { get; set; }
+        [JsonProperty("message-class")]
+        public int? MessageClass { get; set; }
+
+        /// <summary>
+        /// The value of the protocol identifier to use. Ensure that the value is aligned with udh.
+        /// </summary>
+        [JsonProperty("protocol-id")]
+        public int? ProtocolId { get; set; }
+
+        /// <summary>
+        /// Boolean indicating if you like to receive a Delivery Receipt.
+        /// </summary>
+        [JsonProperty("status-report-req")]
+        public bool? StatusReportReq { get; set; }
 
         /// <summary>
         /// The body of the message being sent. If your message contains characters 
@@ -27,6 +80,12 @@ namespace Vonage.Messaging
         /// </summary>
         [JsonProperty("text")]
         public string Text { get; set; }
+
+        /// <summary>
+        /// The number that the message should be sent to. Numbers are specified in E.164 format.
+        /// </summary>
+        [JsonProperty("to")]
+        public string To { get; set; }
 
         /// <summary>
         /// The duration in milliseconds the delivery of an SMS will be attempted.
@@ -38,26 +97,6 @@ namespace Vonage.Messaging
         public int? Ttl { get; set; }
 
         /// <summary>
-        /// Boolean indicating if you like to receive a Delivery Receipt.
-        /// </summary>
-        [JsonProperty("status-report-req")]
-        public bool? StatusReportReq { get; set; }
-
-        /// <summary>
-        /// The webhook endpoint the delivery receipt for this sms is sent to. 
-        /// This parameter overrides the webhook endpoint you set in Dashboard.
-        /// </summary>
-        [JsonProperty("callback")]
-        public string Callback { get; set; }
-
-        /// <summary>
-        ///  The Data Coding Scheme value of the message
-        ///  Must be one of: 0, 1, 2 or 3
-        /// </summary>
-        [JsonProperty("message-class")]
-        public int? MessageClass { get; set; }
-
-        /// <summary>
         /// The format of the message body         
         /// </summary>
         [JsonProperty("type")]
@@ -65,78 +104,9 @@ namespace Vonage.Messaging
         public SmsType? Type { get; set; }
 
         /// <summary>
-        /// A business card in vCard format. Depends on type parameter having the value vcard.
-        /// </summary>
-        [JsonProperty("vcard")]
-        public string Vcard { get; set; }
-
-        /// <summary>
-        /// A calendar event in vCal format. Depends on type parameter having the value vcal.
-        /// </summary>
-        [JsonProperty("vcal")]
-        public string Vcal { get; set; }
-
-        /// <summary>
-        /// Hex encoded binary data. Depends on type parameter having the value binary.
-        /// </summary>
-        [JsonProperty("body")]
-        public string Body { get; set; }
-
-        /// <summary>
         /// Your custom Hex encoded User Data Header. Depends on type parameter having the value binary.
         /// </summary>
         [JsonProperty("udh")]
         public string Udh { get; set; }
-
-        /// <summary>
-        /// The value of the protocol identifier to use. Ensure that the value is aligned with udh.
-        /// </summary>
-        [JsonProperty("protocol-id")]
-        public int? ProtocolId { get; set; }
-
-        /// <summary>
-        /// The title for a wappush SMS. Depends on type parameter having the value wappush.
-        /// </summary>
-        [JsonProperty("title")]
-        public string Title { get; set; }
-
-        /// <summary>
-        /// The URL of your website. Depends on type parameter having the value wappush.
-        /// </summary>
-        [JsonProperty("url")]
-        public string Url { get; set; }
-
-        /// <summary>
-        /// The availability for an SMS in milliseconds. Depends on type parameter having the value wappush.
-        /// </summary>
-        [JsonProperty("validity")]
-        public string Validity { get; set; }
-
-        /// <summary>
-        /// You can optionally include your own reference of up to 40 characters.
-        /// </summary>
-        [JsonProperty("client-ref")]
-        public string ClientRef { get; set; }
-
-        /// <summary>
-        /// An optional string used to identify separate accounts using the SMS endpoint for billing purposes. 
-        /// To use this feature, please email support@nexmo.com
-        /// </summary>
-        [JsonProperty("account-ref")]
-        public string AccountRef { get; set; }
-
-        /// <summary>
-        /// A string parameter that satisfies regulatory requirements when sending an SMS to specific countries.
-        /// For more information please refer to the <see href="https://help.nexmo.com/hc/en-us/articles/115011781468">Country-Specific Outbound SMS Features</see>
-        /// </summary>
-        [JsonProperty("entity-id")]
-        public string EntityId { get; set; }
-
-        /// <summary>
-        /// A string parameter that satisfies regulatory requirements when sending an SMS to specific countries.
-        /// For more information please refer to the <see href="https://help.nexmo.com/hc/en-us/articles/115011781468">Country-Specific Outbound SMS Features</see>
-        /// </summary>
-        [JsonProperty("content-id")]
-        public string ContentId { get; set; }
     }
 }
