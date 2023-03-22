3f95331e7fb5fd8259b6db86d049c5eabc6914f1
tr00d <guillaume.faas@live.fr>
2023-03-22 14:40:44 +0100

    Add builder for start verification request

---
+++ Vonage/VerifyV2/StartVerification/StartVerificationRequestBuilder.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Vonage.VerifyV2.StartVerification;
+
+public class StartVerificationRequestBuilder
+{
+    public static StartVerificationRequestBuilder ForEmail() => throw new NotImplementedException();
+    public static StartVerificationRequestBuilder ForSilentAuth() => throw new NotImplementedException();
+    public static StartVerificationRequestBuilder ForSms() => throw new NotImplementedException();
+    public static StartVerificationRequestBuilder ForVoice() => throw new NotImplementedException();
+    public static StartVerificationRequestBuilder ForWhatsApp() => throw new NotImplementedException();
+    public static StartVerificationRequestBuilder ForWhatsAppInteractive() => throw new NotImplementedException();
+}
