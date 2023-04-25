6fe6cc3f35e9f8e696f15ee191332e7c63a493eb
tr00d <guillaume.faas@live.fr>
2023-04-25 14:45:10 +0200

    Linter broke test project again

--- Vonage.Test.Unit/ProactiveConnect/Items/DeleteItem/UseCaseTest.cs
+++ Vonage.Test.Unit/ProactiveConnect/Items/DeleteItem/UseCaseTest.cs
@@ -13,56 +13,57 @@ using Vonage.ProactiveConnect.Items;
 using Vonage.ProactiveConnect.Items.DeleteItem;
 using Xunit;
 
-namespace Vonage.Test.Unit.ProactiveConnect.Items.DeleteItem;
-
-public class UseCaseTest : BaseUseCase
+namespace Vonage.Test.Unit.ProactiveConnect.Items.DeleteItem
 {
-    private Func<VonageHttpClientConfiguration, Task<Result<ListItem>>> Operation =>
-        configuration => new ProactiveConnectClient(configuration).DeleteItemAsync(this.request);
+    public class UseCaseTest : BaseUseCase
+    {
+        private Func<VonageHttpClientConfiguration, Task<Result<ListItem>>> Operation =>
+            configuration => new ProactiveConnectClient(configuration).DeleteItemAsync(this.request);
 
-    private readonly Result<DeleteItemRequest> request;
+        private readonly Result<DeleteItemRequest> request;
 
-    public UseCaseTest() => this.request = BuildRequest(this.helper.Fixture);
+        public UseCaseTest() => this.request = BuildRequest(this.helper.Fixture);
 
-    [Property]
-    public Property ShouldReturnFailure_GivenApiErrorCannotBeParsed() =>
-        this.helper.VerifyReturnsFailureGivenErrorCannotBeParsed(this.BuildExpectedRequest(), this.Operation);
+        [Property]
+        public Property ShouldReturnFailure_GivenApiErrorCannotBeParsed() =>
+            this.helper.VerifyReturnsFailureGivenErrorCannotBeParsed(this.BuildExpectedRequest(), this.Operation);
 
-    [Fact]
-    public async Task ShouldReturnFailure_GivenApiResponseCannotBeParsed() =>
-        await this.helper.VerifyReturnsFailureGivenApiResponseCannotBeParsed(this.BuildExpectedRequest(),
-            this.Operation);
+        [Fact]
+        public async Task ShouldReturnFailure_GivenApiResponseCannotBeParsed() =>
+            await this.helper.VerifyReturnsFailureGivenApiResponseCannotBeParsed(this.BuildExpectedRequest(),
+                this.Operation);
 
-    [Property]
-    public Property ShouldReturnFailure_GivenApiResponseIsError() =>
-        this.helper.VerifyReturnsFailureGivenApiResponseIsError(this.BuildExpectedRequest(), this.Operation);
+        [Property]
+        public Property ShouldReturnFailure_GivenApiResponseIsError() =>
+            this.helper.VerifyReturnsFailureGivenApiResponseIsError(this.BuildExpectedRequest(), this.Operation);
 
-    [Fact]
-    public async Task ShouldReturnFailure_GivenRequestIsFailure() =>
-        await this.helper
-            .VerifyReturnsFailureGivenRequestIsFailure<DeleteItemRequest, ListItem>(
-                (configuration, failureRequest) =>
-                    new ProactiveConnectClient(configuration).DeleteItemAsync(failureRequest));
+        [Fact]
+        public async Task ShouldReturnFailure_GivenRequestIsFailure() =>
+            await this.helper
+                .VerifyReturnsFailureGivenRequestIsFailure<DeleteItemRequest, ListItem>(
+                    (configuration, failureRequest) =>
+                        new ProactiveConnectClient(configuration).DeleteItemAsync(failureRequest));
 
-    [Fact]
-    public async Task ShouldReturnFailure_GivenTokenGenerationFailed() =>
-        await this.helper.VerifyReturnsFailureGivenTokenGenerationFails(this.Operation);
+        [Fact]
+        public async Task ShouldReturnFailure_GivenTokenGenerationFailed() =>
+            await this.helper.VerifyReturnsFailureGivenTokenGenerationFails(this.Operation);
 
-    [Fact]
-    public async Task ShouldReturnSuccess_GivenApiResponseIsSuccess() =>
-        await this.helper.VerifyReturnsExpectedValueGivenApiResponseIsSuccess(this.BuildExpectedRequest(),
-            this.Operation);
+        [Fact]
+        public async Task ShouldReturnSuccess_GivenApiResponseIsSuccess() =>
+            await this.helper.VerifyReturnsExpectedValueGivenApiResponseIsSuccess(this.BuildExpectedRequest(),
+                this.Operation);
 
-    private ExpectedRequest BuildExpectedRequest() =>
-        new()
-        {
-            Method = HttpMethod.Delete,
-            RequestUri = new Uri(UseCaseHelper.GetPathFromRequest(this.request), UriKind.Relative),
-            Content = this.request
-                .Map(value => this.helper.Serializer.SerializeObject(value))
-                .IfFailure(string.Empty),
-        };
+        private ExpectedRequest BuildExpectedRequest() =>
+            new ExpectedRequest
+            {
+                Method = HttpMethod.Delete,
+                RequestUri = new Uri(UseCaseHelper.GetPathFromRequest(this.request), UriKind.Relative),
+                Content = this.request
+                    .Map(value => this.helper.Serializer.SerializeObject(value))
+                    .IfFailure(string.Empty),
+            };
 
-    private static Result<DeleteItemRequest> BuildRequest(ISpecimenBuilder fixture) =>
-        DeleteItemRequest.Build().WithListId(fixture.Create<Guid>()).WithItemId(fixture.Create<Guid>()).Create();
+        private static Result<DeleteItemRequest> BuildRequest(ISpecimenBuilder fixture) =>
+            DeleteItemRequest.Build().WithListId(fixture.Create<Guid>()).WithItemId(fixture.Create<Guid>()).Create();
+    }
 }
