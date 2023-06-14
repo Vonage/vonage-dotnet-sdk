bfc1a43cce84891aaa78d86290ce7ad89ed7f463
tr00d <guillaume.faas@live.fr>
2023-06-14 10:30:58 +0200

    Implement CreateSubAccountRequest

---
+++ Vonage.Test.Unit/SubAccounts/CreateSubAccount/SerializationTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Vonage.Common;
+using Vonage.Common.Test;
+using Vonage.Common.Test.Extensions;
+using Vonage.SubAccounts;
+using Vonage.SubAccounts.CreateSubAccount;
+using Xunit;
+
+namespace Vonage.Test.Unit.SubAccounts.CreateSubAccount
+{
+    public class SerializationTest
+    {
+        private readonly SerializationTestHelper helper;
+
+        public SerializationTest() =>
+            this.helper = new SerializationTestHelper(
+                typeof(SerializationTest).Namespace,
+                JsonSerializer.BuildWithSnakeCase());
+
+        [Fact]
+        public void ShouldDeserialize200() =>
+            this.helper.Serializer
+                .DeserializeObject<Account>(this.helper.GetResponseJson())
+                .Should()
+                .BeSuccess(new Account(
+                    "aze1243v",
+                    "SubAccount department A",
+                    "bbe6222f",
+                    false,
+                    DateTimeOffset.Parse("2018-03-02T17:34:49Z"),
+                    true,
+                    (decimal) 1.25,
+                    15
+                ));
+
+        [Fact]
+        public void ShouldSerialize() =>
+            CreateSubAccountRequest.Build()
+                .WithName("My SubAccount")
+                .WithSecret("123456789AbcDef")
+                .DisableSharedAccountBalance()
+                .Create()
+                .GetStringContent()
+                .Should()
+                .BeSuccess(this.helper.GetRequestJson());
+    }
+}
---
+++ Vonage/SubAccounts/CreateSubAccount/CreateSubAccountRequest.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Vonage.Common;
+using Vonage.Common.Client;
+using Vonage.Common.Monads;
+
+namespace Vonage.SubAccounts.CreateSubAccount;
+
+/// <inheritdoc />
+public readonly struct CreateSubAccountRequest : IVonageRequest
+{
+    /// <summary>
+    ///     Name of the subaccount.
+    /// </summary>
+    public string Name { get; internal init; }
+
+    /// <summary>
+    ///     Secret of the subaccount.
+    /// </summary>
+    public Maybe<string> Secret { get; internal init; }
+
+    /// <summary>
+    ///     Flag showing if balance is shared with primary account.
+    /// </summary>
+    public bool UsePrimaryAccountBalance { get; internal init; }
+
+    /// <summary>
+    ///     Initializes a builder for CreateRoomRequest.
+    /// </summary>
+    /// <returns>The builder.</returns>
+    public static IBuilderForName Build() => new CreateSubAccountRequestBuilder();
+
+    /// <inheritdoc />
+    public HttpRequestMessage BuildRequestMessage() => VonageRequestBuilder
+        .Initialize(HttpMethod.Post, this.GetEndpointPath())
+        .WithContent(this.GetRequestContent())
+        .Build();
+
+    /// <inheritdoc />
+    public string GetEndpointPath() => $"/accounts/{this.ApiKey}/subaccounts/";
+
+    private StringContent GetRequestContent()
+    {
+        var values = new Dictionary<string, object>
+        {
+            {"name", this.Name},
+            {"use_primary_account_balance", this.UsePrimaryAccountBalance},
+        };
+        this.Secret.IfSome(value => values.Add("secret", value));
+        return new StringContent(JsonSerializer.BuildWithSnakeCase().SerializeObject(values), Encoding.UTF8,
+            "application/json");
+    }
+
+    internal CreateSubAccountRequest WithApiKey(string primaryAccountKey) => this with {ApiKey = primaryAccountKey};
+
+    /// <summary>
+    ///     Unique primary account ID.
+    /// </summary>
+    internal string ApiKey { get; init; }
+}
