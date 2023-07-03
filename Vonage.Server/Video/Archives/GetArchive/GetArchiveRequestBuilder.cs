72a88bd1463097006c93f8f205bef1bb166e445c
tr00d <guillaume.faas@live.fr>
2023-07-03 11:39:18 +0200

    Remove unused class

--- Vonage.Server/Video/Archives/GetArchive/GetArchiveRequestBuilder.cs
+++ Vonage.Server/Video/Archives/GetArchive/GetArchiveRequestBuilder.cs
@@ -1,46 +1,7 @@
 using System;
 using Vonage.Common.Client;
-using Vonage.Common.Monads;
-using Vonage.Common.Validation;
 
 namespace Vonage.Server.Video.Archives.GetArchive;
-
-internal class GetArchiveRequestBuilder : IBuilderForApplicationId, IBuilderForArchiveId,
-    IVonageRequestBuilder<GetArchiveRequest>
-{
-    private Guid applicationId;
-    private Guid archiveId;
-
-    /// <inheritdoc />
-    public Result<GetArchiveRequest> Create() => Result<GetArchiveRequest>
-        .FromSuccess(new GetArchiveRequest
-        {
-            ApplicationId = this.applicationId,
-            ArchiveId = this.archiveId,
-        })
-        .Map(InputEvaluation<GetArchiveRequest>.Evaluate)
-        .Bind(evaluation => evaluation.WithRules(VerifyApplicationId, VerifyArchiveId));
-
-    /// <inheritdoc />
-    public IBuilderForArchiveId WithApplicationId(Guid value)
-    {
-        this.applicationId = value;
-        return this;
-    }
-
-    /// <inheritdoc />
-    public IVonageRequestBuilder<GetArchiveRequest> WithArchiveId(Guid value)
-    {
-        this.archiveId = value;
-        return this;
-    }
-
-    private static Result<GetArchiveRequest> VerifyApplicationId(GetArchiveRequest request) =>
-        InputValidation.VerifyNotEmpty(request, request.ApplicationId, nameof(request.ApplicationId));
-
-    private static Result<GetArchiveRequest> VerifyArchiveId(GetArchiveRequest request) =>
-        InputValidation.VerifyNotEmpty(request, request.ArchiveId, nameof(request.ArchiveId));
-}
 
 /// <summary>
 ///     Represents a builder for ApplicationId.
