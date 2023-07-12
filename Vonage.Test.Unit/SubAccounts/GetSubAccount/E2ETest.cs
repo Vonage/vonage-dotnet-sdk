cc4ca931b32bbe80f744268a26adacce7a567459
tr00d <guillaume.faas@live.fr>
2023-07-12 15:35:50 +0200

    Add E2E for GetSubAccount

--- Vonage.Test.Unit/SubAccounts/GetSubAccount/E2ETest.cs
+++ Vonage.Test.Unit/SubAccounts/GetSubAccount/E2ETest.cs
@@ -2,12 +2,12 @@
 using System.Threading.Tasks;
 using Vonage.Common.Test;
 using Vonage.Common.Test.Extensions;
-using Vonage.SubAccounts.CreateSubAccount;
+using Vonage.SubAccounts.GetSubAccount;
 using Vonage.Test.Unit.TestHelpers;
 using WireMock.ResponseBuilders;
 using Xunit;
 
-namespace Vonage.Test.Unit.SubAccounts.CreateSubAccount
+namespace Vonage.Test.Unit.SubAccounts.GetSubAccount
 {
     [Trait("Category", "E2E")]
     public class E2ETest
@@ -22,21 +22,17 @@ namespace Vonage.Test.Unit.SubAccounts.CreateSubAccount
         }
 
         [Fact]
-        public async Task CreateSubAccount()
+        public async Task GetSubAccount()
         {
             this.helper.Server.Given(WireMock.RequestBuilders.Request.Create()
-                    .WithUrl($"{this.helper.Server.Url}/accounts/790fc5e5/subaccounts")
+                    .WithUrl($"{this.helper.Server.Url}/accounts/790fc5e5/subaccounts/456iFuDL099")
                     .WithHeader("Authorization", "Basic NzkwZmM1ZTU6QWEzNDU2Nzg5")
-                    .WithBody(this.serialization.GetRequestJson(nameof(SerializationTest.ShouldSerialize)))
-                    .UsingPost())
+                    .UsingGet())
                 .RespondWith(Response.Create().WithStatusCode(HttpStatusCode.OK)
                     .WithBody(this.serialization.GetResponseJson(nameof(SerializationTest.ShouldDeserialize200))));
-            var result = await this.helper.VonageClient.SubAccountsClient.CreateSubAccountAsync(CreateSubAccountRequest
-                .Build()
-                .WithName("My SubAccount")
-                .WithSecret("123456789AbcDef")
-                .DisableSharedAccountBalance()
-                .Create());
+            var result =
+                await this.helper.VonageClient.SubAccountsClient.GetSubAccountAsync(
+                    GetSubAccountRequest.Parse("456iFuDL099"));
             result.Should().BeSuccess();
         }
     }
---
+++ Vonage.Test.Unit/SubAccounts/SubAccountsHelper.cs
@@ -0,0 +1,14 @@
+﻿using Vonage.Common;
+using Vonage.Common.Test;
+using Vonage.Test.Unit.TestHelpers;
+
+namespace Vonage.Test.Unit.SubAccounts
+{
+    internal static class SubAccountsHelper
+    {
+        internal static E2EHelper BuildTestHelper() => E2EHelper.WithBasicCredentials("Vonage.Url.Api");
+
+        internal static SerializationTestHelper BuildSerializationHelper(string useCaseNamespace) =>
+            new SerializationTestHelper(useCaseNamespace, JsonSerializer.BuildWithSnakeCase());
+    }
+}
