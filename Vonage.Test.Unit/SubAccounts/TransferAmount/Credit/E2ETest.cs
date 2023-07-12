5eb162d4b88811ac091a2eb4764f9dab8241113b
tr00d <guillaume.faas@live.fr>
2023-07-12 17:10:32 +0200

    Add E2E for TransferAmount

---
+++ Vonage.Test.Unit/SubAccounts/TransferAmount/Credit/E2ETest.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Vonage.Common.Test;
+using Vonage.Common.Test.Extensions;
+using Vonage.SubAccounts.TransferAmount;
+using Vonage.Test.Unit.TestHelpers;
+using WireMock.ResponseBuilders;
+using Xunit;
+
+namespace Vonage.Test.Unit.SubAccounts.TransferAmount.Credit
+{
+    [Trait("Category", "E2E")]
+    public class E2ETest
+    {
+        private readonly E2EHelper helper;
+        private readonly SerializationTestHelper serialization;
+        private readonly SerializationTestHelper serializationRequest;
+
+        public E2ETest()
+        {
+            this.helper = SubAccountsHelper.BuildTestHelper();
+            this.serialization = SubAccountsHelper.BuildSerializationHelper(typeof(SerializationTest).Namespace);
+            this.serializationRequest =
+                SubAccountsHelper.BuildSerializationHelper(
+                    typeof(TransferAmount.SerializationTest).Namespace);
+        }
+
+        [Fact]
+        public async Task TransferAmount()
+        {
+            this.helper.Server.Given(WireMock.RequestBuilders.Request.Create()
+                    .WithPath("/accounts/790fc5e5/credit-transfers")
+                    .WithHeader("Authorization", "Basic NzkwZmM1ZTU6QWEzNDU2Nzg5")
+                    .WithBody(this.serializationRequest.GetRequestJson(nameof(SubAccounts.TransferAmount
+                        .SerializationTest.ShouldSerialize)))
+                    .UsingPost())
+                .RespondWith(Response.Create().WithStatusCode(HttpStatusCode.OK)
+                    .WithBody(this.serialization.GetResponseJson(nameof(SerializationTest.ShouldDeserialize200))));
+            var result = await this.helper.VonageClient.SubAccountsClient.TransferCreditAsync(TransferAmountRequest
+                .Build()
+                .WithFrom("7c9738e6")
+                .WithTo("ad6dc56f")
+                .WithAmount((decimal) 123.45)
+                .WithReference("This gets added to the audit log")
+                .Create());
+            result.Should().BeSuccess();
+        }
+    }
+}
