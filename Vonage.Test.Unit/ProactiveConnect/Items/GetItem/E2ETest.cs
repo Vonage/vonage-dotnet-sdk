ce9c28e11e930ffe46f065b9231960fe5440de63
tr00d <guillaume.faas@live.fr>
2023-07-13 14:16:19 +0200

    Add E2E for GetItem

--- Vonage.Test.Unit/ProactiveConnect/Items/GetItem/E2ETest.cs
+++ Vonage.Test.Unit/ProactiveConnect/Items/GetItem/E2ETest.cs
@@ -1,9 +1,35 @@
-﻿namespace Vonage.Test.Unit.ProactiveConnect.Items.GetItem
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Vonage.Common.Test.Extensions;
+using Vonage.ProactiveConnect.Items.GetItem;
+using WireMock.ResponseBuilders;
+using Xunit;
+
+namespace Vonage.Test.Unit.ProactiveConnect.Items.GetItem
 {
     public class E2ETest : E2EBase
     {
         public E2ETest() : base(typeof(SerializationTest).Namespace)
         {
+        }
+
+        [Fact]
+        public async Task GetItem()
+        {
+            this.helper.Server.Given(WireMock.RequestBuilders.Request.Create()
+                    .WithPath(
+                        "/v0.1/bulk/lists/95a462d3-ed87-4aa5-9d91-098e08093b0b/items/0f3e672d-e60e-4869-9eac-fce9047532b5")
+                    .WithHeader("Authorization", "Bearer *")
+                    .UsingPost())
+                .RespondWith(Response.Create().WithStatusCode(HttpStatusCode.OK)
+                    .WithBody(this.serialization.GetResponseJson(nameof(SerializationTest.ShouldDeserialize200))));
+            var result = await this.helper.VonageClient.ProactiveConnectClient.GetItemAsync(GetItemRequest
+                .Build()
+                .WithListId(new Guid("95a462d3-ed87-4aa5-9d91-098e08093b0b"))
+                .WithItemId(new Guid("0f3e672d-e60e-4869-9eac-fce9047532b5"))
+                .Create());
+            result.Should().BeSuccess();
         }
     }
 }
