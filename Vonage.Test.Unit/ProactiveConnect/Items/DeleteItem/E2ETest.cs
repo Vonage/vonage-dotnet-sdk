7762cdc5f9fd8f4c7d6fcb6bebf8066bbd6e6974
tr00d <guillaume.faas@live.fr>
2023-07-13 14:08:30 +0200

    Add E2E for DeleteItem

--- Vonage.Test.Unit/ProactiveConnect/Items/DeleteItem/E2ETest.cs
+++ Vonage.Test.Unit/ProactiveConnect/Items/DeleteItem/E2ETest.cs
@@ -1,4 +1,11 @@
-﻿using Vonage.Test.Unit.ProactiveConnect.Items.CreateItem;
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Vonage.Common.Test.Extensions;
+using Vonage.ProactiveConnect.Items.DeleteItem;
+using Vonage.Test.Unit.ProactiveConnect.Items.CreateItem;
+using WireMock.ResponseBuilders;
+using Xunit;
 
 namespace Vonage.Test.Unit.ProactiveConnect.Items.DeleteItem
 {
@@ -6,6 +13,23 @@ namespace Vonage.Test.Unit.ProactiveConnect.Items.DeleteItem
     {
         public E2ETest() : base(typeof(SerializationTest).Namespace)
         {
+        }
+
+        [Fact]
+        public async Task DeleteItem()
+        {
+            this.helper.Server.Given(WireMock.RequestBuilders.Request.Create()
+                    .WithPath(
+                        "/v0.1/bulk/lists/95a462d3-ed87-4aa5-9d91-098e08093b0b/items/0f3e672d-e60e-4869-9eac-fce9047532b5")
+                    .WithHeader("Authorization", "Bearer *")
+                    .UsingDelete())
+                .RespondWith(Response.Create().WithStatusCode(HttpStatusCode.OK));
+            var result = await this.helper.VonageClient.ProactiveConnectClient.DeleteItemAsync(DeleteItemRequest
+                .Build()
+                .WithListId(new Guid("95a462d3-ed87-4aa5-9d91-098e08093b0b"))
+                .WithItemId(new Guid("0f3e672d-e60e-4869-9eac-fce9047532b5"))
+                .Create());
+            result.Should().BeSuccess();
         }
     }
 }
