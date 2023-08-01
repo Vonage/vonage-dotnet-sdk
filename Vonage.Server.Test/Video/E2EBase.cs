394d42b921ab2bb73c8f38c1ea1c8f08f2e8c442
tr00d <guillaume.faas@live.fr>
2023-08-01 09:26:07 +0200

    Add E2E for AddStream

---
+++ Vonage.Server.Test/Video/E2EBase.cs
@@ -0,0 +1,17 @@
+﻿using Vonage.Common;
+using Vonage.Common.Test;
+using Vonage.Server.Test.TestHelpers;
+
+namespace Vonage.Server.Test.Video
+{
+    public abstract class E2EBase
+    {
+        private E2EBase() => this.Helper = E2EHelper.WithBearerCredentials("Vonage.Url.Api.Video");
+
+        protected E2EBase(string serializationNamespace) : this() => this.Serialization =
+            new SerializationTestHelper(serializationNamespace, JsonSerializer.BuildWithCamelCase());
+
+        internal readonly E2EHelper Helper;
+        internal readonly SerializationTestHelper Serialization;
+    }
+}
