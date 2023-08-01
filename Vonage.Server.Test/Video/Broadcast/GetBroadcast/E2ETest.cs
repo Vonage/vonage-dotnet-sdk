460ce9e375430b60dfa5bbd8bd4b5870e450ca43
Guillaume Faas <59444272+Tr00d@users.noreply.github.com>
2023-08-01 19:09:34 +0200

    refactor: video e2e refactoring (#476)
    * Add E2E for AddStreamToBroadcast
    
    * Add E2E for ChangeBroadcastLayout
    
    * Add E2E test for GetBroadcast
    
    * Add E2E for GetBroadcasts
    
    * Add E2E for RemoveStreamFromBroadcast
    
    * Add E2E for StartBroadcast
    
    * Add E2E for StartBroadcast

---
+++ Vonage.Server.Test/Video/Broadcast/GetBroadcast/E2ETest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Vonage.Common.Test.Extensions;
+using Vonage.Server.Video.Broadcast.GetBroadcast;
+using WireMock.ResponseBuilders;
+using Xunit;
+
+namespace Vonage.Server.Test.Video.Broadcast.GetBroadcast
+{
+    [Trait("Category", "E2E")]
+    public class E2ETest : E2EBase
+    {
+        public E2ETest() : base(typeof(E2ETest).Namespace)
+        {
+        }
+
+        [Fact]
+        public async Task GetBroadcast()
+        {
+            this.Helper.Server.Given(WireMock.RequestBuilders.Request.Create()
+                    .WithPath(
+                        "/v2/project/5e782e3b-9f63-426f-bd2e-b7d618d546cd/broadcast/97425ae1-4722-4dbf-b395-6169f08ebab3")
+                    .WithHeader("Authorization", "Bearer *")
+                    .UsingGet())
+                .RespondWith(Response.Create().WithStatusCode(HttpStatusCode.OK)
+                    .WithBody(this.Serialization.GetResponseJson(nameof(SerializationTest.ShouldDeserialize200))));
+            await this.Helper.VonageClient.BroadcastClient.GetBroadcastAsync(GetBroadcastRequest
+                    .Build()
+                    .WithApplicationId(Guid.Parse("5e782e3b-9f63-426f-bd2e-b7d618d546cd"))
+                    .WithBroadcastId(Guid.Parse("97425ae1-4722-4dbf-b395-6169f08ebab3"))
+                    .Create())
+                .Should()
+                .BeSuccessAsync(SerializationTest.VerifyBroadcast);
+        }
+    }
+}
---
+++ Vonage.Server.Test/Video/Broadcast/GetBroadcasts/E2ETest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Vonage.Common.Test.Extensions;
+using Vonage.Server.Video.Broadcast.GetBroadcasts;
+using WireMock.ResponseBuilders;
+using Xunit;
+
+namespace Vonage.Server.Test.Video.Broadcast.GetBroadcasts
+{
+    [Trait("Category", "E2E")]
+    public class E2ETest : E2EBase
+    {
+        public E2ETest() : base(typeof(E2ETest).Namespace)
+        {
+        }
+
+        [Fact]
+        public async Task GetBroadcasts()
+        {
+            this.Helper.Server.Given(WireMock.RequestBuilders.Request.Create()
+                    .WithPath(
+                        "/v2/project/5e782e3b-9f63-426f-bd2e-b7d618d546cd/broadcast")
+                    .WithParam("offset", "1000")
+                    .WithParam("count", "100")
+                    .WithParam("sessionId", "flR1ZSBPY3QgMjkgMTI6MTM6MjMgUERUIDIwMTN")
+                    .WithHeader("Authorization", "Bearer *")
+                    .UsingGet())
+                .RespondWith(Response.Create().WithStatusCode(HttpStatusCode.OK)
+                    .WithBody(this.Serialization.GetResponseJson(nameof(SerializationTest.ShouldDeserialize200))));
+            await this.Helper.VonageClient.BroadcastClient.GetBroadcastsAsync(GetBroadcastsRequest
+                    .Build()
+                    .WithApplicationId(Guid.Parse("5e782e3b-9f63-426f-bd2e-b7d618d546cd"))
+                    .WithCount(100)
+                    .WithOffset(1000)
+                    .WithSessionId("flR1ZSBPY3QgMjkgMTI6MTM6MjMgUERUIDIwMTN")
+                    .Create())
+                .Should()
+                .BeSuccessAsync(SerializationTest.VerifyBroadcasts);
+        }
+    }
+}
