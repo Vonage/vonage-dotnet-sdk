a91f2ed013f08a82ed16a30ce5386ca2530db3d8
tr00d <guillaume.faas@live.fr>
2023-08-01 15:35:20 +0200

    Add E2E for GetArchive

---
+++ Vonage.Server.Test/Video/Archives/GetArchive/E2ETest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Vonage.Common.Test.Extensions;
+using Vonage.Server.Video.Archives.GetArchive;
+using WireMock.ResponseBuilders;
+using Xunit;
+
+namespace Vonage.Server.Test.Video.Archives.GetArchive
+{
+    [Trait("Category", "E2E")]
+    public class E2ETest : E2EBase
+    {
+        public E2ETest() : base(typeof(E2ETest).Namespace)
+        {
+        }
+
+        [Fact]
+        public async Task GetArchive()
+        {
+            this.Helper.Server.Given(WireMock.RequestBuilders.Request.Create()
+                    .WithPath(
+                        "/v2/project/5e782e3b-9f63-426f-bd2e-b7d618d546cd/archive/97425ae1-4722-4dbf-b395-6169f08ebab3")
+                    .WithHeader("Authorization", "Bearer *")
+                    .UsingGet())
+                .RespondWith(Response.Create().WithStatusCode(HttpStatusCode.OK)
+                    .WithBody(this.Serialization.GetResponseJson(nameof(SerializationTest.ShouldDeserialize200))));
+            await this.Helper.VonageClient.ArchiveClient.GetArchiveAsync(GetArchiveRequest.Build()
+                    .WithApplicationId(Guid.Parse("5e782e3b-9f63-426f-bd2e-b7d618d546cd"))
+                    .WithArchiveId(Guid.Parse("97425ae1-4722-4dbf-b395-6169f08ebab3"))
+                    .Create())
+                .Should()
+                .BeSuccessAsync(SerializationTest.VerifyArchive);
+        }
+    }
+}
