bd22c5e80bc94302bb4bee346fb1ff420595d63b
Guillaume Faas <59444272+Tr00d@users.noreply.github.com>
2023-08-02 14:14:00 +0200

    refactor: use case helpers (#478)

---
+++ Vonage.Common.Test/ExpectedRequest.cs
@@ -0,0 +1,11 @@
+﻿using Vonage.Common.Monads;
+
+namespace Vonage.Common.Test
+{
+    public struct ExpectedRequest
+    {
+        public Maybe<string> Content { get; set; }
+        public HttpMethod Method { get; set; }
+        public Uri RequestUri { get; set; }
+    }
+}
--- Vonage.Common.Test/TestHelpers/IUseCaseWithResponse.cs
+++
@@ -1,6 +0,0 @@
-namespace Vonage.Common.Test.TestHelpers;
-
-public interface IUseCaseWithResponse : IUseCase
-{
-    Task ShouldReturnFailure_GivenApiResponseCannotBeParsed();
-}
