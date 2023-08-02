161e5ec691534c90300b63004312bec1bf19175d
tr00d <guillaume.faas@live.fr>
2023-08-02 08:49:34 +0200

    Add E2E for MuteStream

--- Vonage.Server.Test/Video/Moderation/MuteStream/SerializationTest.cs
+++ Vonage.Server.Test/Video/Moderation/MuteStream/SerializationTest.cs
@@ -21,13 +21,15 @@ namespace Vonage.Server.Test.Video.Moderation.MuteStream
         public void ShouldDeserialize200() =>
             this.helper.Serializer.DeserializeObject<MuteStreamResponse>(this.helper.GetResponseJson())
                 .Should()
-                .BeSuccess(success =>
-                {
-                    success.ApplicationId.Should().Be("78d335fa-323d-0114-9c3d-d6f0d48968cf");
-                    success.Status.Should().Be("ACTIVE");
-                    success.Name.Should().Be("Joe Montana");
-                    success.Environment.Should().Be("standard");
-                    success.CreatedAt.Should().Be(1414642898000);
-                });
+                .BeSuccess(VerifyResponse);
+
+        internal static void VerifyResponse(MuteStreamResponse success)
+        {
+            success.ApplicationId.Should().Be("78d335fa-323d-0114-9c3d-d6f0d48968cf");
+            success.Status.Should().Be("ACTIVE");
+            success.Name.Should().Be("Joe Montana");
+            success.Environment.Should().Be("standard");
+            success.CreatedAt.Should().Be(1414642898000);
+        }
     }
 }
