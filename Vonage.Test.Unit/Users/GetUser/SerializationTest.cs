3f53542ce9cba0dfb4060ac112ba2158e30d422f
tr00d <guillaume.faas@live.fr>
2023-08-03 11:54:11 +0200

    Fix linter issue

--- Vonage.Test.Unit/Users/GetUser/SerializationTest.cs
+++ Vonage.Test.Unit/Users/GetUser/SerializationTest.cs
@@ -11,7 +11,7 @@ namespace Vonage.Test.Unit.Users.GetUser
 {
     public class SerializationTest
     {
-        private readonly SerializationTestHelper helper = new(
+        private readonly SerializationTestHelper helper = new SerializationTestHelper(
             typeof(SerializationTest).Namespace,
             JsonSerializer.BuildWithSnakeCase());
 
