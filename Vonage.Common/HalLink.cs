e924835dfbf5c0b69ad32088cd0ccc0fdf331af9
tr00d <guillaume.faas@live.fr>
2023-08-07 15:27:08 +0200

    Implement GetUsersRequest creation from HAL Link

--- Vonage.Common/HalLink.cs
+++ Vonage.Common/HalLink.cs
@@ -41,6 +41,43 @@ public struct HalLinks
 }
 
 /// <summary>
+///     Represents a set of HAL Links.
+/// </summary>
+public struct HalLinks<T>
+{
+    /// <summary>
+    ///     Represents the navigation link to the first element.
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public T First { get; set; }
+
+    /// <summary>
+    ///     Represents the navigation link to the last element.
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public T Last { get; set; }
+
+    /// <summary>
+    ///     Represents the navigation link to the next element.
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public T Next { get; set; }
+
+    /// <summary>
+    ///     Represents the navigation link to the previous element.
+    /// </summary>
+    [JsonPropertyName("prev")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public T Previous { get; set; }
+
+    /// <summary>
+    ///     Represents the navigation link to the current element.
+    /// </summary>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public T Self { get; set; }
+}
+
+/// <summary>
 ///     Represents a link to another page.
 /// </summary>
 /// <param name="Href">Hyperlink reference.</param>
--- Vonage/Users/GetUsers/GetUsersResponse.cs
+++ Vonage/Users/GetUsers/GetUsersResponse.cs
@@ -3,6 +3,7 @@ using System.Text.Json.Serialization;
 using EnumsNET;
 using Microsoft.AspNetCore.WebUtilities;
 using Vonage.Common;
+using Vonage.Common.Failures;
 using Vonage.Common.Monads;
 using Vonage.ProactiveConnect;
 
@@ -15,14 +16,44 @@ public record GetUsersResponse(
     EmbeddedUsers Embedded,
     [property: JsonPropertyName("_links")]
     [property: JsonPropertyOrder(2)]
-    HalLinks Links)
+    HalLinks<GetUsersHalLink> Links);
+
+/// <summary>
+///     Represents a link to another resource.
+/// </summary>
+/// <param name="Href">Hyperlink reference.</param>
+public record GetUsersHalLink(Uri Href)
 {
-    public Result<GetUsersRequest> BuildRequestForNext()
+    /// <summary>
+    ///     Transforms the link into a GetUsersRequest using the cursor pagination.
+    /// </summary>
+    /// <returns></returns>
+    public Result<GetUsersRequest> BuildRequest()
     {
-        var a = QueryHelpers.ParseQuery(this.Links.Next.Href.Query);
-        return Result<GetUsersRequest>.FromSuccess(new GetUsersRequest(a["cursor"].ToString(), a["name"].ToString(),Enums.Parse<FetchOrder>(a["order"].ToString(), false, EnumFormat.Description), int.Parse(a["page_size"].ToString()) ));
+        var queryValues = QueryHelpers.ParseQuery(this.Href.Query);
+        var name = queryValues.TryGetValue("name", out var value)
+            ? Maybe<string>.Some(value.ToString())
+            : Maybe<string>.None;
+        if (!queryValues.ContainsKey("page_size"))
+        {
+            return Result<GetUsersRequest>.FromFailure(ResultFailure.FromErrorMessage("PageSize is missing from Uri."));
+        }
+
+        if (!queryValues.ContainsKey("order"))
+        {
+            return Result<GetUsersRequest>.FromFailure(ResultFailure.FromErrorMessage("Order is missing from Uri."));
+        }
+
+        if (!queryValues.ContainsKey("cursor"))
+        {
+            return Result<GetUsersRequest>.FromFailure(ResultFailure.FromErrorMessage("Cursor is missing from Uri."));
+        }
+
+        return Result<GetUsersRequest>.FromSuccess(new GetUsersRequest(queryValues["cursor"].ToString(), name,
+            Enums.Parse<FetchOrder>(queryValues["order"].ToString(), false, EnumFormat.Description),
+            int.Parse(queryValues["page_size"].ToString())));
     }
-};
+}
 
 public record EmbeddedUsers(UserSummary[] Users);
 
