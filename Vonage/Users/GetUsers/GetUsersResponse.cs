3af38e23cb2bb18501d0045e89478e8d46a461b1
tr00d <guillaume.faas@live.fr>
2023-08-09 17:01:10 +0200

    Add missing xml doc

--- Vonage/Users/GetUsers/GetUsersResponse.cs
+++ Vonage/Users/GetUsers/GetUsersResponse.cs
@@ -11,9 +11,9 @@ namespace Vonage.Users.GetUsers;
 
 /// <summary>
 /// </summary>
-/// <param name="PageSize"></param>
-/// <param name="Embedded"></param>
-/// <param name="Links"></param>
+/// <param name="PageSize">The amount of records returned in this response.</param>
+/// <param name="Embedded">A list of user objects. See <see href="https://developer.vonage.com/en/api/application.v2#getUser">the get details</see> of a specific user response fields for a description of the nested objects</param>
+/// <param name="Links">A series of links between resources in this API in the http://stateless.co/hal_specification.html.</param>
 public record GetUsersResponse(
     [property: JsonPropertyOrder(0)] int PageSize,
     [property: JsonPropertyName("_embedded")]
@@ -59,16 +59,18 @@ public record GetUsersHalLink(Uri Href)
 }
 
 /// <summary>
+/// Represents a list of user objects.
 /// </summary>
-/// <param name="Users"></param>
+/// <param name="Users">List of users matching the provided filter.</param>
 public record EmbeddedUsers(UserSummary[] Users);
 
 /// <summary>
+/// Represents a user summary.
 /// </summary>
-/// <param name="Id"></param>
-/// <param name="Name"></param>
-/// <param name="DisplayName"></param>
-/// <param name="Links"></param>
+/// <param name="Id">User ID</param>
+/// <param name="Name">Unique name for a user</param>
+/// <param name="DisplayName">A string to be displayed as user name. It does not need to be unique</param>
+/// <param name="Links">A series of links between resources in this API in the http://stateless.co/hal_specification.html.</param>
 public record UserSummary(
     [property: JsonPropertyOrder(0)] string Id,
     [property: JsonPropertyName("name")]
