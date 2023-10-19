024ea9954630f9acfec2a5299256a7d8c6bd1bbb
Guillaume Faas <guillaume.faas@live.fr>
2023-10-19 07:07:17 +0200

    refactor: fix typos and clean WebhookParser

--- Vonage/Utility/WebhookParser.cs
+++ Vonage/Utility/WebhookParser.cs
@@ -10,11 +10,14 @@ using Newtonsoft.Json;
 
 namespace Vonage.Utility;
 
+/// <summary>
+///     Utility class to parse Webhook results.
+/// </summary>
 public class WebhookParser
 {
     /// <summary>
-    /// Used to Parse Query parameters into a givent type
-    /// This Method will convert the string pairs into a dictioanry and then use
+    /// Used to Parse Query parameters into a given type
+    /// This Method will convert the string pairs into a dictionary and then use
     /// Newtonsoft to convert the pairs to JSON - finally resolving the object from JSON
     /// </summary>
     /// <typeparam name="T"></typeparam>
@@ -28,8 +31,8 @@ public class WebhookParser
     }
 
     /// <summary>
-    /// Used to Parse Query parameters into a givent type
-    /// This Method will convert the string pairs into a dictioanry and then use
+    /// Used to Parse Query parameters into a given type
+    /// This Method will convert the string pairs into a dictionary and then use
     /// Newtonsoft to convert the pairs to JSON - finally resolving the object from JSON
     /// </summary>
     /// <typeparam name="T"></typeparam>
@@ -51,15 +54,10 @@ public class WebhookParser
     /// <returns></returns>
     public static T ParseUrlFormString<T>(string contentString)
     {
-        var split_parameters = contentString.Split(new[] {'&'});
-        var content_dictonary = new Dictionary<string, string>();
-        foreach (var param in split_parameters)
-        {
-            var split = param.Split('=');
-            content_dictonary.Add(split[0], WebUtility.UrlDecode(split[1]));
-        }
-
-        var json = JsonConvert.SerializeObject(content_dictonary);
+        var splitParameters = contentString.Split('&');
+        var contentDictionary = splitParameters.Select(param => param.Split('='))
+            .ToDictionary(split => split[0], split => WebUtility.UrlDecode(split[1]));
+        var json = JsonConvert.SerializeObject(contentDictionary);
         return JsonConvert.DeserializeObject<T>(json);
     }
 
@@ -69,23 +67,19 @@ public class WebhookParser
     /// </summary>
     /// <typeparam name="T"></typeparam>
     /// <param name="content"></param>
+    /// <param name="contentType"></param>
     /// <returns></returns>
-    public static T ParseWebhook<T>(Stream content, string contentType)
-    {
-        return ParseWebhookAsync<T>(content, contentType).Result;
-    }
+    public static T ParseWebhook<T>(Stream content, string contentType) =>
+        ParseWebhookAsync<T>(content, contentType).Result;
 
     /// <summary>
-    /// Synchronous implementation of the ParseWehbook method, meant to be called from 
+    /// Synchronous implementation of the ParseWebhook method, meant to be called from 
     /// Legacy ASP.NET Web Api with an HttpRequestMessage
     /// </summary>
     /// <typeparam name="T"></typeparam>
     /// <param name="request"></param>
     /// <returns></returns>
-    public static T ParseWebhook<T>(HttpRequestMessage request)
-    {
-        return ParseWebhookAsync<T>(request).Result;
-    }
+    public static T ParseWebhook<T>(HttpRequestMessage request) => ParseWebhookAsync<T>(request).Result;
 
     /// <summary>
     /// Parses the stream into the given type
@@ -93,6 +87,7 @@ public class WebhookParser
     /// </summary>
     /// <typeparam name="T"></typeparam>
     /// <param name="request"></param>
+    /// <param name="content"></param>
     /// <param name="contentType">The content type of the request, must be of the type application/json or application/x-www-form-urlencoded</param>
     /// <exception cref="ArgumentException">Thrown if Content type does not contain application/json or application/x-www-form-urlencoded</exception>
     /// <returns></returns>
@@ -100,27 +95,23 @@ public class WebhookParser
     {
         if (contentType.Contains("application/json"))
         {
-            using (var reader = new StreamReader(content))
-            {
-                var json = await reader.ReadToEndAsync();
-                return JsonConvert.DeserializeObject<T>(json);
-            }
+            using var reader = new StreamReader(content);
+            var json = await reader.ReadToEndAsync();
+            return JsonConvert.DeserializeObject<T>(json);
         }
 
         if (contentType.Contains("application/x-www-form-urlencoded"))
         {
-            using (var reader = new StreamReader(content))
-            {
-                var contentString = await reader.ReadToEndAsync();
-                return ParseUrlFormString<T>(contentString);
-            }
+            using var reader = new StreamReader(content);
+            var contentString = await reader.ReadToEndAsync();
+            return ParseUrlFormString<T>(contentString);
         }
 
         throw new ArgumentException("Invalid Content Type");
     }
 
     /// <summary>
-    /// Parses the HttpRequestMessag's content into the given type
+    /// Parses the HttpRequestMessage's content into the given type
     /// </summary>
     /// <typeparam name="T"></typeparam>
     /// <param name="request"></param>
