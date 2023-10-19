78c7898321f151dc32e150176e33ee6937ee1197
Guillaume Faas <guillaume.faas@live.fr>
2023-10-19 06:55:07 +0200

    Delete unnecessary class WebhookTypeDictionaryConverter

--- Vonage/Applications/Capabilities/Capability.cs
+++ Vonage/Applications/Capabilities/Capability.cs
@@ -1,16 +1,13 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
 using Newtonsoft.Json;
 using Vonage.Common;
-using Vonage.Serialization;
 
 namespace Vonage.Applications.Capabilities;
 
 public abstract class Capability
 {
-    [JsonProperty("webhooks")]
-    [JsonConverter(typeof(WebhookTypeDictionaryConverter))]
-    public IDictionary<Webhook.Type, Webhook> Webhooks { get; set; }
+    [JsonProperty("webhooks")] public IDictionary<Webhook.Type, Webhook> Webhooks { get; set; }
 
     [JsonIgnore] protected CapabilityType Type { get; set; }
 
