795a1ee972d8602c70cd6a5b1d2b9fa59ad0e72d
Guillaume Faas <guillaume.faas@live.fr>
2023-10-30 15:58:16 +0100

    Remove sync methods from VerifyClient

--- Vonage/Verify/IVerifyClient.cs
+++ Vonage/Verify/IVerifyClient.cs
@@ -6,14 +6,6 @@ namespace Vonage.Verify;
 public interface IVerifyClient
 {
     /// <summary>
-    /// Use Verify request to generate and send a PIN to your user
-    /// </summary>
-    /// <param name="request"></param>
-    /// <param name="creds"></param>
-    /// <returns></returns>
-    Task<VerifyResponse> VerifyRequestAsync(VerifyRequest request, Credentials creds = null);
-
-    /// <summary>
     /// Use Verify check to confirm that the PIN you received from your user matches the one sent by Vonage in your Verify request
     /// </summary>
     /// <param name="request"></param>
@@ -22,20 +14,20 @@ public interface IVerifyClient
     Task<VerifyCheckResponse> VerifyCheckAsync(VerifyCheckRequest request, Credentials creds = null);
 
     /// <summary>
-    /// Use Verify search to check the status of past or current verification requests
-    /// </summary>
-    /// <param name="request"></param>
-    /// <param name="creds"></param>
-    /// <returns></returns>
-    Task<VerifySearchResponse> VerifySearchAsync(VerifySearchRequest request, Credentials creds = null);
-
-    /// <summary>
     /// Control the progress of your Verify requests. To cancel an existing Verify request, or to trigger the next verification event
     /// </summary>
     /// <param name="request"></param>
     /// <param name="creds"></param>
     /// <returns></returns>
     Task<VerifyControlResponse> VerifyControlAsync(VerifyControlRequest request, Credentials creds = null);
+
+    /// <summary>
+    /// Use Verify request to generate and send a PIN to your user
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="creds"></param>
+    /// <returns></returns>
+    Task<VerifyResponse> VerifyRequestAsync(VerifyRequest request, Credentials creds = null);
 
     /// <summary>
     /// Use Verify request to generate and send a PIN to your user to authorize a payment: 
@@ -49,45 +41,10 @@ public interface IVerifyClient
     Task<VerifyResponse> VerifyRequestWithPSD2Async(Psd2Request request, Credentials creds = null);
 
     /// <summary>
-    /// Use Verify request to generate and send a PIN to your user
-    /// </summary>
-    /// <param name="request"></param>
-    /// <param name="creds"></param>
-    /// <returns></returns>
-    VerifyResponse VerifyRequest(VerifyRequest request, Credentials creds = null);
-
-    /// <summary>
-    /// Use Verify check to confirm that the PIN you received from your user matches the one sent by Vonage in your Verify request
-    /// </summary>
-    /// <param name="request"></param>
-    /// <param name="creds"></param>
-    /// <returns></returns>
-    VerifyCheckResponse VerifyCheck(VerifyCheckRequest request, Credentials creds = null);
-
-    /// <summary>
     /// Use Verify search to check the status of past or current verification requests
     /// </summary>
     /// <param name="request"></param>
     /// <param name="creds"></param>
     /// <returns></returns>
-    VerifySearchResponse VerifySearch(VerifySearchRequest request, Credentials creds = null);
-
-    /// <summary>
-    /// Control the progress of your Verify requests. To cancel an existing Verify request, or to trigger the next verification event
-    /// </summary>
-    /// <param name="request"></param>
-    /// <param name="creds"></param>
-    /// <returns></returns>
-    VerifyControlResponse VerifyControl(VerifyControlRequest request, Credentials creds = null);
-
-    /// <summary>
-    /// Use Verify request to generate and send a PIN to your user to authorize a payment: 
-    /// 1. Create a request to send a verification code to your user. 
-    /// 2. Check the status field in the response to ensure that your request was successful (zero is success). 
-    /// 3. Use the request_id field in the response for the Verify check.
-    /// </summary>
-    /// <param name="request"></param>
-    /// <param name="creds"></param>
-    /// <returns></returns>
-    VerifyResponse VerifyRequestWithPSD2(Psd2Request request, Credentials creds = null);
+    Task<VerifySearchResponse> VerifySearchAsync(VerifySearchRequest request, Credentials creds = null);
 }
