d27c3c06867be48bd0450c5d78e2d37cf77840ad
Guillaume Faas <guillaume.faas@live.fr>
2023-10-30 15:02:21 +0100

    Remove sync methods from PricingClient

--- Vonage.Test.Unit/PricingTests.cs
+++ Vonage.Test.Unit/PricingTests.cs
@@ -1,4 +1,6 @@
-﻿using Vonage.Request;
+﻿using System.Threading.Tasks;
+using Vonage.Pricing;
+using Vonage.Request;
 using Xunit;
 
 namespace Vonage.Test.Unit
@@ -8,7 +10,7 @@ namespace Vonage.Test.Unit
         [Theory]
         [InlineData(true)]
         [InlineData(false)]
-        public void GetPricingAllCountries(bool passCreds)
+        public async Task GetPricingAllCountriesAsync(bool passCreds)
         {
             //ARRANGE
             var expectedUri =
@@ -41,69 +43,7 @@ namespace Vonage.Test.Unit
             //ACT
             var creds = Credentials.FromApiKeyAndSecret(this.ApiKey, this.ApiSecret);
             var client = this.BuildVonageClient(creds);
-            Pricing.PricingResult pricing;
-            if (passCreds)
-            {
-                pricing = client.PricingClient.RetrievePricingAllCountries("sms", creds);
-            }
-            else
-            {
-                pricing = client.PricingClient.RetrievePricingAllCountries("sms");
-            }
-
-            //ASSERT
-            Assert.Equal("302530", pricing.Countries[0].Networks[0].NetworkCode);
-            Assert.Equal("Keewaytinook Okimakanak", pricing.Countries[0].Networks[0].NetworkName);
-            Assert.Equal("530", pricing.Countries[0].Networks[0].Mnc);
-            Assert.Equal("302", pricing.Countries[0].Networks[0].Mcc);
-            Assert.Equal("EUR", pricing.Countries[0].Networks[0].Currency);
-            Assert.Equal("0.00590000", pricing.Countries[0].Networks[0].Price);
-            Assert.Equal("mobile", pricing.Countries[0].Networks[0].Type);
-            Assert.Equal("1", pricing.Countries[0].DialingPrefix);
-            Assert.Equal("0.00620000", pricing.Countries[0].DefaultPrice);
-            Assert.Equal("EUR", pricing.Countries[0].Currency);
-            Assert.Equal("Canada", pricing.Countries[0].CountryDisplayName);
-            Assert.Equal("Canada", pricing.Countries[0].CountryName);
-            Assert.Equal("243", pricing.Count);
-        }
-
-        [Theory]
-        [InlineData(true)]
-        [InlineData(false)]
-        public async void GetPricingAllCountriesAsync(bool passCreds)
-        {
-            //ARRANGE
-            var expectedUri =
-                $"{this.RestUrl}/account/get-pricing/outbound/sms?api_key={this.ApiKey}&api_secret={this.ApiSecret}&";
-            var expectedResponse = @"{
-                  ""count"": ""243"",
-                  ""countries"": [
-                    {
-                      ""countryName"": ""Canada"",
-                      ""countryDisplayName"": ""Canada"",
-                      ""currency"": ""EUR"",
-                      ""defaultPrice"": ""0.00620000"",
-                      ""dialingPrefix"": ""1"",
-                      ""networks"": [
-                        {
-                          ""type"": ""mobile"",
-                          ""price"": ""0.00590000"",
-                          ""currency"": ""EUR"",
-                          ""mcc"": ""302"",
-                          ""mnc"": ""530"",
-                          ""networkCode"": ""302530"",
-                          ""networkName"": ""Keewaytinook Okimakanak""
-                        }
-                      ]
-                    }
-                  ]
-                }";
-            this.Setup(uri: expectedUri, responseContent: expectedResponse);
-
-            //ACT
-            var creds = Credentials.FromApiKeyAndSecret(this.ApiKey, this.ApiSecret);
-            var client = this.BuildVonageClient(creds);
-            Pricing.PricingResult pricing;
+            PricingResult pricing;
             if (passCreds)
             {
                 pricing = await client.PricingClient.RetrievePricingAllCountriesAsync("sms", creds);
@@ -132,7 +72,7 @@ namespace Vonage.Test.Unit
         [Theory]
         [InlineData(true)]
         [InlineData(false)]
-        public void GetPricingForCountry(bool passCreds)
+        public async Task GetPricingForCountryAsync(bool passCreds)
         {
             //ARRANGE
             var expectedUri =
@@ -161,16 +101,16 @@ namespace Vonage.Test.Unit
             //ACT
             var creds = Credentials.FromApiKeyAndSecret(this.ApiKey, this.ApiSecret);
             var client = this.BuildVonageClient(creds);
-            Pricing.Country country;
+            Country country;
             if (passCreds)
             {
-                country = client.PricingClient.RetrievePricingCountry("sms",
-                    new Pricing.PricingCountryRequest {Country = "CA"}, creds);
+                country = await client.PricingClient.RetrievePricingCountryAsync("sms",
+                    new PricingCountryRequest {Country = "CA"}, creds);
             }
             else
             {
-                country = client.PricingClient.RetrievePricingCountry("sms",
-                    new Pricing.PricingCountryRequest {Country = "CA"});
+                country = await client.PricingClient.RetrievePricingCountryAsync("sms",
+                    new PricingCountryRequest {Country = "CA"});
             }
 
             //ASSERT
@@ -192,67 +132,7 @@ namespace Vonage.Test.Unit
         [Theory]
         [InlineData(true)]
         [InlineData(false)]
-        public async void GetPricingForCountryAsync(bool passCreds)
-        {
-            //ARRANGE
-            var expectedUri =
-                $"{this.RestUrl}/account/get-pricing/outbound/sms?country=CA&api_key={this.ApiKey}&api_secret={this.ApiSecret}&";
-            var expectedResponseContent = @"{
-              ""countryCode"": ""CA"",
-              ""countryName"": ""Canada"",
-              ""countryDisplayName"": ""Canada"",
-              ""currency"": ""EUR"",
-              ""defaultPrice"": ""0.00620000"",
-              ""dialingPrefix"": ""1"",
-              ""networks"": [
-                {
-                  ""type"": ""mobile"",
-                  ""price"": ""0.00590000"",
-                  ""currency"": ""EUR"",
-                  ""mcc"": ""302"",
-                  ""mnc"": ""530"",
-                  ""networkCode"": ""302530"",
-                  ""networkName"": ""Keewaytinook Okimakanak""
-                }
-              ]
-            }";
-            this.Setup(uri: expectedUri, responseContent: expectedResponseContent);
-
-            //ACT
-            var creds = Credentials.FromApiKeyAndSecret(this.ApiKey, this.ApiSecret);
-            var client = this.BuildVonageClient(creds);
-            Pricing.Country country;
-            if (passCreds)
-            {
-                country = await client.PricingClient.RetrievePricingCountryAsync("sms",
-                    new Pricing.PricingCountryRequest {Country = "CA"}, creds);
-            }
-            else
-            {
-                country = await client.PricingClient.RetrievePricingCountryAsync("sms",
-                    new Pricing.PricingCountryRequest {Country = "CA"});
-            }
-
-            //ASSERT
-            Assert.Equal("302530", country.Networks[0].NetworkCode);
-            Assert.Equal("Keewaytinook Okimakanak", country.Networks[0].NetworkName);
-            Assert.Equal("530", country.Networks[0].Mnc);
-            Assert.Equal("302", country.Networks[0].Mcc);
-            Assert.Equal("EUR", country.Networks[0].Currency);
-            Assert.Equal("0.00590000", country.Networks[0].Price);
-            Assert.Equal("mobile", country.Networks[0].Type);
-            Assert.Equal("1", country.DialingPrefix);
-            Assert.Equal("0.00620000", country.DefaultPrice);
-            Assert.Equal("EUR", country.Currency);
-            Assert.Equal("Canada", country.CountryDisplayName);
-            Assert.Equal("Canada", country.CountryName);
-            Assert.Equal("CA", country.CountryCode);
-        }
-
-        [Theory]
-        [InlineData(true)]
-        [InlineData(false)]
-        public void GetPricingForPrefix(bool passCreds)
+        public async Task GetPricingForPrefixAsync(bool passCreds)
         {
             //ARRANGE
             var expectedUri =
@@ -285,80 +165,16 @@ namespace Vonage.Test.Unit
             //ACT
             var creds = Credentials.FromApiKeyAndSecret(this.ApiKey, this.ApiSecret);
             var client = this.BuildVonageClient(creds);
-            Pricing.PricingResult pricing;
-            if (passCreds)
-            {
-                pricing = client.PricingClient.RetrievePrefixPricing("sms",
-                    new Pricing.PricingPrefixRequest {Prefix = "1"}, creds);
-            }
-            else
-            {
-                pricing = client.PricingClient.RetrievePrefixPricing("sms",
-                    new Pricing.PricingPrefixRequest {Prefix = "1"});
-            }
-
-            //ASSERT
-            Assert.Equal("302530", pricing.Countries[0].Networks[0].NetworkCode);
-            Assert.Equal("Keewaytinook Okimakanak", pricing.Countries[0].Networks[0].NetworkName);
-            Assert.Equal("530", pricing.Countries[0].Networks[0].Mnc);
-            Assert.Equal("302", pricing.Countries[0].Networks[0].Mcc);
-            Assert.Equal("EUR", pricing.Countries[0].Networks[0].Currency);
-            Assert.Equal("0.00590000", pricing.Countries[0].Networks[0].Price);
-            Assert.Equal("mobile", pricing.Countries[0].Networks[0].Type);
-            Assert.Equal("1", pricing.Countries[0].DialingPrefix);
-            Assert.Equal("0.00620000", pricing.Countries[0].DefaultPrice);
-            Assert.Equal("EUR", pricing.Countries[0].Currency);
-            Assert.Equal("Canada", pricing.Countries[0].CountryDisplayName);
-            Assert.Equal("Canada", pricing.Countries[0].CountryName);
-            Assert.Equal("243", pricing.Count);
-        }
-
-        [Theory]
-        [InlineData(true)]
-        [InlineData(false)]
-        public async void GetPricingForPrefixAsync(bool passCreds)
-        {
-            //ARRANGE
-            var expectedUri =
-                $"{this.RestUrl}/account/get-prefix-pricing/outbound/sms?prefix=1&api_key={this.ApiKey}&api_secret={this.ApiSecret}&";
-            var expectedResponse = @"{
-                  ""count"": ""243"",
-                  ""countries"": [
-                    {
-                      ""countryName"": ""Canada"",
-                      ""countryDisplayName"": ""Canada"",
-                      ""currency"": ""EUR"",
-                      ""defaultPrice"": ""0.00620000"",
-                      ""dialingPrefix"": ""1"",
-                      ""networks"": [
-                        {
-                          ""type"": ""mobile"",
-                          ""price"": ""0.00590000"",
-                          ""currency"": ""EUR"",
-                          ""mcc"": ""302"",
-                          ""mnc"": ""530"",
-                          ""networkCode"": ""302530"",
-                          ""networkName"": ""Keewaytinook Okimakanak""
-                        }
-                      ]
-                    }
-                  ]
-                }";
-            this.Setup(uri: expectedUri, responseContent: expectedResponse);
-
-            //ACT
-            var creds = Credentials.FromApiKeyAndSecret(this.ApiKey, this.ApiSecret);
-            var client = this.BuildVonageClient(creds);
-            Pricing.PricingResult pricing;
+            PricingResult pricing;
             if (passCreds)
             {
                 pricing = await client.PricingClient.RetrievePrefixPricingAsync("sms",
-                    new Pricing.PricingPrefixRequest {Prefix = "1"}, creds);
+                    new PricingPrefixRequest {Prefix = "1"}, creds);
             }
             else
             {
                 pricing = await client.PricingClient.RetrievePrefixPricingAsync("sms",
-                    new Pricing.PricingPrefixRequest {Prefix = "1"});
+                    new PricingPrefixRequest {Prefix = "1"});
             }
 
             //ASSERT
--- Vonage/Pricing/IPricingClient.cs
+++ Vonage/Pricing/IPricingClient.cs
@@ -6,14 +6,14 @@ namespace Vonage.Pricing;
 public interface IPricingClient
 {
     /// <summary>
-    /// Retrieves the pricing information based on the specified country.
+    /// Retrieves the pricing information based on the dialing prefix.
     /// </summary>
     /// <param name="type">The type of service you wish to retrieve data about: 
     /// either sms, sms-transit or voice.</param>
     /// <param name="request"></param>
     /// <param name="creds"></param>
     /// <returns></returns>
-    Task<Country> RetrievePricingCountryAsync(string type, PricingCountryRequest request, Credentials creds = null);
+    Task<PricingResult> RetrievePrefixPricingAsync(string type, PricingPrefixRequest request, Credentials creds = null);
 
     /// <summary>
     /// Retrieves the pricing information for all countries.
@@ -25,16 +25,6 @@ public interface IPricingClient
     Task<PricingResult> RetrievePricingAllCountriesAsync(string type, Credentials creds = null);
 
     /// <summary>
-    /// Retrieves the pricing information based on the dialing prefix.
-    /// </summary>
-    /// <param name="type">The type of service you wish to retrieve data about: 
-    /// either sms, sms-transit or voice.</param>
-    /// <param name="request"></param>
-    /// <param name="creds"></param>
-    /// <returns></returns>
-    Task<PricingResult> RetrievePrefixPricingAsync(string type, PricingPrefixRequest request, Credentials creds = null);
-
-    /// <summary>
     /// Retrieves the pricing information based on the specified country.
     /// </summary>
     /// <param name="type">The type of service you wish to retrieve data about: 
@@ -42,24 +32,5 @@ public interface IPricingClient
     /// <param name="request"></param>
     /// <param name="creds"></param>
     /// <returns></returns>
-    Country RetrievePricingCountry(string type, PricingCountryRequest request, Credentials creds = null);
-
-    /// <summary>
-    /// Retrieves the pricing information for all countries.
-    /// </summary>
-    /// <param name="type">The type of service you wish to retrieve data about: 
-    /// either sms, sms-transit or voice.</param>
-    /// <param name="creds"></param>
-    /// <returns></returns>
-    PricingResult RetrievePricingAllCountries(string type, Credentials creds = null);
-
-    /// <summary>
-    /// Retrieves the pricing information based on the dialing prefix.
-    /// </summary>
-    /// <param name="type">The type of service you wish to retrieve data about: 
-    /// either sms, sms-transit or voice.</param>
-    /// <param name="request"></param>
-    /// <param name="creds"></param>
-    /// <returns></returns>
-    PricingResult RetrievePrefixPricing(string type, PricingPrefixRequest request, Credentials creds = null);
+    Task<Country> RetrievePricingCountryAsync(string type, PricingCountryRequest request, Credentials creds = null);
 }
