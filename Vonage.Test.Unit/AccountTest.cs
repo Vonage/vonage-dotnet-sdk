303b8e91d9984083667bedfeffc96414fbbf1727
Guillaume Faas <guillaume.faas@live.fr>
2023-10-30 07:48:00 +0100

    Remove sync methods and obsolete subaccounts methods from AccountClient

--- Vonage.Test.Unit/AccountTest.cs
+++ Vonage.Test.Unit/AccountTest.cs
@@ -12,49 +12,6 @@ namespace Vonage.Test.Unit
         [Theory]
         [InlineData(false)]
         [InlineData(true)]
-        public void CreateApiSecret(bool passCreds)
-        {
-            //ARRANGE            
-            var expectedUri = $"https://api.nexmo.com/accounts/{this.ApiKey}/secrets";
-            var expectedResponse = @"{
-                  ""_links"": {
-                    ""self"": {
-                           ""href"": ""abc123""
-                      }
-                    },
-                  ""id"": ""ad6dc56f-07b5-46e1-a527-85530e625800"",
-                  ""created_at"": ""2017-03-02T16:34:49Z""
-                }";
-            this.Setup(expectedUri, expectedResponse);
-
-            //ACT
-            var creds = Credentials.FromApiKeyAndSecret(this.ApiKey, this.ApiSecret);
-            var client = this.BuildVonageClient(creds);
-            Secret secret;
-            if (passCreds)
-            {
-                secret = client.AccountClient.CreateApiSecret(new CreateSecretRequest {Secret = "password"},
-                    this.ApiKey, creds);
-            }
-            else
-            {
-                secret = client.AccountClient.CreateApiSecret(new CreateSecretRequest {Secret = "password"},
-                    this.ApiKey);
-            }
-
-            //ASSERT
-            Assert.Equal("ad6dc56f-07b5-46e1-a527-85530e625800", secret.Id);
-            Assert.Equal("2017-03-02T16:34:49Z", secret.CreatedAt);
-            Assert.Equal("abc123", secret.Links.Self.Href);
-            Assert.Null(secret.Links.Next);
-            Assert.Null(secret.Links.Prev);
-            Assert.Null(secret.Links.First);
-            Assert.Null(secret.Links.Last);
-        }
-
-        [Theory]
-        [InlineData(false)]
-        [InlineData(true)]
         public async void CreateApiSecretAsync(bool passCreds)
         {
             //ARRANGE            
@@ -94,32 +51,6 @@ namespace Vonage.Test.Unit
         [Theory]
         [InlineData(false)]
         [InlineData(true)]
-        public void GetAccountBalance(bool passCreds)
-        {
-            //ARRANGE
-            var expectedUri = $"{this.RestUrl}/account/get-balance?api_key={this.ApiKey}&api_secret={this.ApiSecret}&";
-            var expectedResponseContent = @"{""value"": 3.14159, ""autoReload"": false }";
-            this.Setup(expectedUri, expectedResponseContent);
-            var creds = Credentials.FromApiKeyAndSecret(this.ApiKey, this.ApiSecret);
-            var client = this.BuildVonageClient(creds);
-            Balance balance;
-            if (passCreds)
-            {
-                balance = client.AccountClient.GetAccountBalance(creds);
-            }
-            else
-            {
-                balance = client.AccountClient.GetAccountBalance();
-            }
-
-            //ASSERT
-            Assert.Equal(3.14159m, balance.Value);
-            Assert.False(balance.AutoReload);
-        }
-
-        [Theory]
-        [InlineData(false)]
-        [InlineData(true)]
         public async void GetAccountBalanceAsync(bool passCreds)
         {
             //ARRANGE
@@ -144,7 +75,7 @@ namespace Vonage.Test.Unit
         }
 
         [Fact]
-        public async void GetAccountBalanceAsync_ShouldReturnbalance_GivenCredentialsContainSecuritySecret()
+        public async void GetAccountBalanceAsync_ShouldReturnBalance_GivenCredentialsContainSecuritySecret()
         {
             var mockTimeProvider = new Mock<ITimeProvider>();
             mockTimeProvider.Setup(provider => provider.Epoch).Returns(10);
@@ -158,55 +89,6 @@ namespace Vonage.Test.Unit
             var balance = await client.AccountClient.GetAccountBalanceAsync();
             Assert.Equal(3.14159m, balance.Value);
             Assert.False(balance.AutoReload);
-        }
-
-        [Theory]
-        [InlineData(false)]
-        [InlineData(true)]
-        public void RetrieveApiSecrets(bool passCreds)
-        {
-            //ARRANGE            
-            var expectedResponse = @"{
-                  ""_links"": {
-                    ""self"": {
-                        ""href"": ""abc123""
-                      }
-                  },
-                  ""_embedded"": {
-                    ""secrets"": [
-                      {
-                        ""_links"": {
-                          ""self"": {
-                            ""href"": ""abc123""
-                          }
-                        },
-                        ""id"": ""ad6dc56f-07b5-46e1-a527-85530e625800"",
-                        ""created_at"": ""2017-03-02T16:34:49Z""
-                      }
-                    ]
-                  }
-                }";
-            var expectedUri = $"https://api.nexmo.com/accounts/{this.ApiKey}/secrets";
-            this.Setup(expectedUri, expectedResponse);
-
-            //ACT
-            var creds = Credentials.FromApiKeyAndSecret(this.ApiKey, this.ApiSecret);
-            var client = this.BuildVonageClient(creds);
-            SecretsRequestResult secrets;
-            if (passCreds)
-            {
-                secrets = client.AccountClient.RetrieveApiSecrets(this.ApiKey, creds);
-            }
-            else
-            {
-                secrets = client.AccountClient.RetrieveApiSecrets(this.ApiKey);
-            }
-
-            //ASSERT
-            Assert.Equal("ad6dc56f-07b5-46e1-a527-85530e625800", secrets.Embedded.Secrets[0].Id);
-            Assert.Equal("2017-03-02T16:34:49Z", secrets.Embedded.Secrets[0].CreatedAt);
-            Assert.Equal("abc123", secrets.Embedded.Secrets[0].Links.Self.Href);
-            Assert.Equal("abc123", secrets.Links.Self.Href);
         }
 
         [Theory]
@@ -261,44 +143,6 @@ namespace Vonage.Test.Unit
         [Theory]
         [InlineData(false)]
         [InlineData(true)]
-        public void RetrieveSecret(bool passCreds)
-        {
-            //ARRANGE            
-            var secretId = "ad6dc56f-07b5-46e1-a527-85530e625800";
-            var expectedUri = $"https://api.nexmo.com/accounts/{this.ApiKey}/secrets/{secretId}";
-            var expectedResponse = @"{
-                  ""_links"": {
-                    ""self"": {
-                           ""href"": ""abc123""
-                      }
-                    },
-                  ""id"": ""ad6dc56f-07b5-46e1-a527-85530e625800"",
-                  ""created_at"": ""2017-03-02T16:34:49Z""
-                }";
-            this.Setup(expectedUri, expectedResponse);
-
-            //ACT
-            var creds = Credentials.FromApiKeyAndSecret(this.ApiKey, this.ApiSecret);
-            var client = this.BuildVonageClient(creds);
-            Secret secret;
-            if (passCreds)
-            {
-                secret = client.AccountClient.RetrieveApiSecret(secretId, this.ApiKey, creds);
-            }
-            else
-            {
-                secret = client.AccountClient.RetrieveApiSecret(secretId, this.ApiKey);
-            }
-
-            //ASSERT
-            Assert.Equal(secretId, secret.Id);
-            Assert.Equal("2017-03-02T16:34:49Z", secret.CreatedAt);
-            Assert.Equal("abc123", secret.Links.Self.Href);
-        }
-
-        [Theory]
-        [InlineData(false)]
-        [InlineData(true)]
         public async void RetrieveSecretAsync(bool passCreds)
         {
             //ARRANGE            
@@ -337,34 +181,6 @@ namespace Vonage.Test.Unit
         [Theory]
         [InlineData(false)]
         [InlineData(true)]
-        public void RevokeSecret(bool passCreds)
-        {
-            //ARRANGE            
-            var secretId = "ad6dc56f-07b5-46e1-a527-85530e625800";
-            var expectedUri = $"https://api.nexmo.com/accounts/{this.ApiKey}/secrets/{secretId}";
-            var expectedResponse = @"";
-            this.Setup(expectedUri, expectedResponse);
-
-            //ACT
-            var creds = Credentials.FromApiKeyAndSecret(this.ApiKey, this.ApiSecret);
-            var client = this.BuildVonageClient(creds);
-            bool response;
-            if (passCreds)
-            {
-                response = client.AccountClient.RevokeApiSecret(secretId, this.ApiKey, creds);
-            }
-            else
-            {
-                response = client.AccountClient.RevokeApiSecret(secretId, this.ApiKey);
-            }
-
-            //ASSERT
-            Assert.True(response);
-        }
-
-        [Theory]
-        [InlineData(false)]
-        [InlineData(true)]
         public async void RevokeSecretAsync(bool passCreds)
         {
             //ARRANGE            
@@ -388,50 +204,6 @@ namespace Vonage.Test.Unit
 
             //ASSERT
             Assert.True(response);
-        }
-
-        [Theory]
-        [InlineData(false)]
-        [InlineData(true)]
-        public void SetSettings(bool passCreds)
-        {
-            //ARRANGE
-            var expectedUri = $"{this.RestUrl}/account/settings";
-            var expectedRequestContents =
-                $"moCallBackUrl={WebUtility.UrlEncode("https://example.com/webhooks/inbound-sms")}&drCallBackUrl={WebUtility.UrlEncode("https://example.com/webhooks/delivery-receipt")}&api_key={this.ApiKey}&api_secret={this.ApiSecret}&";
-            var expectedResponseContent =
-                @"{""mo-callback-url"": ""https://example.com/webhooks/inbound-sms"",""dr-callback-url"": ""https://example.com/webhooks/delivery-receipt"",""max-outbound-request"": 15,""max-inbound-request"": 30,""max-calls-per-second"": 4}";
-            this.Setup(expectedUri, expectedResponseContent,
-                expectedRequestContents);
-
-            //ACT
-            var creds = Credentials.FromApiKeyAndSecret(this.ApiKey, this.ApiSecret);
-            var client = this.BuildVonageClient(creds);
-            AccountSettingsResult result;
-            if (passCreds)
-            {
-                result = client.AccountClient.ChangeAccountSettings(
-                    new AccountSettingsRequest
-                    {
-                        MoCallBackUrl = "https://example.com/webhooks/inbound-sms",
-                        DrCallBackUrl = "https://example.com/webhooks/delivery-receipt",
-                    }, creds);
-            }
-            else
-            {
-                result = client.AccountClient.ChangeAccountSettings(new AccountSettingsRequest
-                {
-                    MoCallBackUrl = "https://example.com/webhooks/inbound-sms",
-                    DrCallBackUrl = "https://example.com/webhooks/delivery-receipt",
-                });
-            }
-
-            //ASSERT
-            Assert.Equal("https://example.com/webhooks/delivery-receipt", result.DrCallbackurl);
-            Assert.Equal("https://example.com/webhooks/inbound-sms", result.MoCallbackUrl);
-            Assert.Equal(4, result.MaxCallsPerSecond);
-            Assert.Equal(30, result.MaxInboundRequest);
-            Assert.Equal(15, result.MaxOutboundRequest);
         }
 
         [Theory]
@@ -476,35 +248,6 @@ namespace Vonage.Test.Unit
             Assert.Equal(4, result.MaxCallsPerSecond);
             Assert.Equal(30, result.MaxInboundRequest);
             Assert.Equal(15, result.MaxOutboundRequest);
-        }
-
-        [Theory]
-        [InlineData(false)]
-        [InlineData(true)]
-        public void TopUp(bool passCreds)
-        {
-            //ARRANGE            
-            var expectedUri =
-                $"{this.RestUrl}/account/top-up?trx=00X123456Y7890123Z&api_key={this.ApiKey}&api_secret={this.ApiSecret}&";
-            var expectedResponseContent = @"{""response"":""abc123""}";
-            this.Setup(expectedUri, expectedResponseContent);
-            var creds = Credentials.FromApiKeyAndSecret(this.ApiKey, this.ApiSecret);
-
-            //Act
-            var client = this.BuildVonageClient(creds);
-            TopUpResult response;
-            if (passCreds)
-            {
-                response = client.AccountClient.TopUpAccountBalance(
-                    new TopUpRequest {Trx = "00X123456Y7890123Z"}, creds);
-            }
-            else
-            {
-                response = client.AccountClient.TopUpAccountBalance(new TopUpRequest
-                    {Trx = "00X123456Y7890123Z"});
-            }
-
-            Assert.Equal("abc123", response.Response);
         }
 
         [Theory]
--- Vonage/Accounts/AccountClient.cs
+++ Vonage/Accounts/AccountClient.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
 using Vonage.Common;
 using Vonage.Request;
@@ -26,16 +25,6 @@ public class AccountClient : IAccountClient
     }
 
     /// <inheritdoc/>
-    [Obsolete("Favor asynchronous version instead.")]
-    public AccountSettingsResult ChangeAccountSettings(AccountSettingsRequest request, Credentials creds = null) =>
-        ApiRequest.Build(this.GetCredentials(creds), this.configuration, this.timeProvider)
-            .DoPostRequestUrlContentFromObject<AccountSettingsResult>
-            (
-                ApiRequest.GetBaseUriFor("/account/settings"),
-                request
-            );
-
-    /// <inheritdoc/>
     public Task<AccountSettingsResult> ChangeAccountSettingsAsync(AccountSettingsRequest request,
         Credentials creds = null) =>
         ApiRequest.Build(this.GetCredentials(creds), this.configuration, this.timeProvider)
@@ -43,17 +32,6 @@ public class AccountClient : IAccountClient
             (
                 ApiRequest.GetBaseUriFor("/account/settings"),
                 request
-            );
-
-    /// <inheritdoc/>
-    [Obsolete("Favor asynchronous version instead.")]
-    public Secret CreateApiSecret(CreateSecretRequest request, string apiKey = null, Credentials creds = null) =>
-        ApiRequest.Build(this.GetCredentials(creds), this.configuration, this.timeProvider)
-            .DoRequestWithJsonContent<Secret>(
-                HttpMethod.Post,
-                ApiRequest.GetBaseUri(ApiRequest.UriType.Api, $"/accounts/{apiKey}/secrets"),
-                request,
-                AuthType.Basic
             );
 
     /// <inheritdoc/>
@@ -68,46 +46,6 @@ public class AccountClient : IAccountClient
             );
 
     /// <inheritdoc/>
-    [Obsolete("Use SubAccountsClient instead.")]
-    public SubAccount CreateSubAccount(CreateSubAccountRequest request, string apiKey = null,
-        Credentials creds = null)
-    {
-        var credentials = this.GetCredentials(creds);
-        var accountId = apiKey ?? credentials.ApiKey;
-        return ApiRequest.Build(this.GetCredentials(creds), this.configuration, this.timeProvider)
-            .DoRequestWithJsonContent<SubAccount>(
-                HttpMethod.Post,
-                ApiRequest.GetBaseUri(ApiRequest.UriType.Api, $"/accounts/{accountId}/subaccounts"),
-                request,
-                AuthType.Basic
-            );
-    }
-
-    /// <inheritdoc/>
-    [Obsolete("Use SubAccountsClient instead.")]
-    public Task<SubAccount> CreateSubAccountAsync(CreateSubAccountRequest request, string apiKey = null,
-        Credentials creds = null)
-    {
-        var credentials = this.GetCredentials(creds);
-        var accountId = apiKey ?? credentials.ApiKey;
-        return ApiRequest.Build(this.GetCredentials(creds), this.configuration, this.timeProvider)
-            .DoRequestWithJsonContentAsync<SubAccount>(
-                HttpMethod.Post,
-                ApiRequest.GetBaseUri(ApiRequest.UriType.Api, $"/accounts/{accountId}/subaccounts"),
-                request,
-                AuthType.Basic
-            );
-    }
-
-    /// <inheritdoc/>
-    [Obsolete("Favor asynchronous version instead.")]
-    public Balance GetAccountBalance(Credentials creds = null) =>
-        ApiRequest.Build(this.GetCredentials(creds), this.configuration, this.timeProvider)
-            .DoGetRequestWithQueryParameters<Balance>(
-                ApiRequest.GetBaseUriFor("/account/get-balance"),
-                AuthType.Query);
-
-    /// <inheritdoc/>
     public Task<Balance> GetAccountBalanceAsync(Credentials creds = null) =>
         ApiRequest.Build(this.GetCredentials(creds), this.configuration, this.timeProvider)
             .DoGetRequestWithQueryParametersAsync<Balance>(
@@ -115,28 +53,10 @@ public class AccountClient : IAccountClient
                 AuthType.Query);
 
     /// <inheritdoc/>
-    [Obsolete("Favor asynchronous version instead.")]
-    public Secret RetrieveApiSecret(string secretId, string apiKey = null, Credentials creds = null) =>
-        ApiRequest.Build(this.GetCredentials(creds), this.configuration, this.timeProvider)
-            .DoGetRequestWithQueryParameters<Secret>(
-                ApiRequest.GetBaseUri(ApiRequest.UriType.Api, $"/accounts/{apiKey}/secrets/{secretId}"),
-                AuthType.Basic
-            );
-
-    /// <inheritdoc/>
     public Task<Secret> RetrieveApiSecretAsync(string secretId, string apiKey = null, Credentials creds = null) =>
         ApiRequest.Build(this.GetCredentials(creds), this.configuration, this.timeProvider)
             .DoGetRequestWithQueryParametersAsync<Secret>(
                 ApiRequest.GetBaseUri(ApiRequest.UriType.Api, $"/accounts/{apiKey}/secrets/{secretId}"),
-                AuthType.Basic
-            );
-
-    /// <inheritdoc/>
-    [Obsolete("Favor asynchronous version instead.")]
-    public SecretsRequestResult RetrieveApiSecrets(string apiKey = null, Credentials creds = null) =>
-        ApiRequest.Build(this.GetCredentials(creds), this.configuration, this.timeProvider)
-            .DoGetRequestWithQueryParameters<SecretsRequestResult>(
-                ApiRequest.GetBaseUri(ApiRequest.UriType.Api, $"/accounts/{apiKey}/secrets"),
                 AuthType.Basic
             );
 
@@ -149,46 +69,6 @@ public class AccountClient : IAccountClient
             );
 
     /// <inheritdoc/>
-    [Obsolete("Use SubAccountsClient instead.")]
-    public SubAccount RetrieveSubAccount(string subAccountKey, string apiKey = null, Credentials creds = null)
-    {
-        var credentials = this.GetCredentials(creds);
-        var accountId = apiKey ?? credentials.ApiKey;
-        return ApiRequest.Build(this.GetCredentials(creds), this.configuration, this.timeProvider)
-            .DoGetRequestWithQueryParameters<SubAccount>(
-                ApiRequest.GetBaseUri(ApiRequest.UriType.Api, $"/accounts/{accountId}/subaccounts/{subAccountKey}"),
-                AuthType.Basic
-            );
-    }
-
-    /// <inheritdoc/>
-    [Obsolete("Use SubAccountsClient instead.")]
-    public Task<SubAccount> RetrieveSubAccountAsync(string subAccountKey, string apiKey = null,
-        Credentials creds = null)
-    {
-        var credentials = this.GetCredentials(creds);
-        var accountId = apiKey ?? credentials.ApiKey;
-        return ApiRequest.Build(this.GetCredentials(creds), this.configuration, this.timeProvider)
-            .DoGetRequestWithQueryParametersAsync<SubAccount>(
-                ApiRequest.GetBaseUri(ApiRequest.UriType.Api, $"/accounts/{accountId}/subaccounts/{subAccountKey}"),
-                AuthType.Basic
-            );
-    }
-
-    /// <inheritdoc/>
-    [Obsolete("Favor asynchronous version instead.")]
-    public bool RevokeApiSecret(string secretId, string apiKey = null, Credentials creds = null)
-    {
-        ApiRequest.Build(this.GetCredentials(creds), this.configuration, this.timeProvider)
-            .DoDeleteRequestWithUrlContent(
-                ApiRequest.GetBaseUri(ApiRequest.UriType.Api, $"/accounts/{apiKey}/secrets/{secretId}"),
-                null,
-                AuthType.Basic
-            );
-        return true;
-    }
-
-    /// <inheritdoc/>
     public async Task<bool> RevokeApiSecretAsync(string secretId, string apiKey = null, Credentials creds = null)
     {
         await ApiRequest.Build(this.GetCredentials(creds), this.configuration, this.timeProvider)
@@ -199,16 +79,6 @@ public class AccountClient : IAccountClient
             );
         return true;
     }
-
-    /// <inheritdoc/>
-    [Obsolete("Favor asynchronous version instead.")]
-    public TopUpResult TopUpAccountBalance(TopUpRequest request, Credentials creds = null) =>
-        ApiRequest.Build(this.GetCredentials(creds), this.configuration, this.timeProvider)
-            .DoGetRequestWithQueryParameters<TopUpResult>(
-                ApiRequest.GetBaseUriFor("/account/top-up"),
-                AuthType.Query,
-                request
-            );
 
     /// <inheritdoc/>
     public Task<TopUpResult> TopUpAccountBalanceAsync(TopUpRequest request, Credentials creds = null) =>
