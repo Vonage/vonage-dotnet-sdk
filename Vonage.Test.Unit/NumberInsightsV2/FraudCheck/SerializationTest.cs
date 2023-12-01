4309d110dc9eed4c911d76f74a8839df9873b373
Guillaume Faas <guillaume.faas@live.fr>
2023-12-01 10:39:57 +0100

    feat: FraudCheck without FraudScore or SimSwap

--- Vonage.Test.Unit/NumberInsightsV2/FraudCheck/SerializationTest.cs
+++ Vonage.Test.Unit/NumberInsightsV2/FraudCheck/SerializationTest.cs
@@ -21,6 +21,18 @@ namespace Vonage.Test.Unit.NumberInsightsV2.FraudCheck
             .BeSuccess(GetExpectedFraudCheckResponse());
 
         [Fact]
+        public void ShouldDeserialize200WithoutFraudScore() => this.helper.Serializer
+            .DeserializeObject<FraudCheckResponse>(this.helper.GetResponseJson())
+            .Should()
+            .BeSuccess(GetExpectedFraudCheckResponseWithoutFraudScore());
+
+        [Fact]
+        public void ShouldDeserialize200WithoutSimSwap() => this.helper.Serializer
+            .DeserializeObject<FraudCheckResponse>(this.helper.GetResponseJson())
+            .Should()
+            .BeSuccess(GetExpectedFraudCheckResponseWithoutSimSwap());
+
+        [Fact]
         public void ShouldSerializeWithFraudScore() => BuildRequestWithFraudScore()
             .GetStringContent()
             .Should()
@@ -37,6 +49,26 @@ namespace Vonage.Test.Unit.NumberInsightsV2.FraudCheck
             .GetStringContent()
             .Should()
             .BeSuccess(this.helper.GetRequestJson());
+
+        private static FraudCheckResponse GetExpectedFraudCheckResponseWithoutFraudScore() => new FraudCheckResponse(
+            new Guid("6cb4c489-0fc8-4c40-8c3d-95e7e74f9450"),
+            "phone",
+            new PhoneData("16197363066", "Orange France", "MOBILE"),
+            Maybe<FraudScore>.None,
+            Maybe<SimSwap>.Some(new SimSwap(SimSwapStatus.Completed, true, "Mobile Network Operator Not Supported")));
+
+        private static FraudCheckResponse GetExpectedFraudCheckResponseWithoutSimSwap() => new FraudCheckResponse(
+            new Guid("6cb4c489-0fc8-4c40-8c3d-95e7e74f9450"),
+            "phone",
+            new PhoneData("16197363066", "Orange France", "MOBILE"),
+            Maybe<FraudScore>.Some(new FraudScore("54", RiskRecommendation.Block, FraudScoreLabel.Low, "completed")),
+            Maybe<SimSwap>.None);
+
+        internal static Result<FraudCheckRequest> BuildRequestWithFraudScore() =>
+            FraudCheckRequest.Build()
+                .WithPhone("447009000000")
+                .WithFraudScore()
+                .Create();
 
         internal static Result<FraudCheckRequest> BuildRequestWithFraudScoreAndSimSwap() =>
             FraudCheckRequest.Build()
@@ -57,11 +89,5 @@ namespace Vonage.Test.Unit.NumberInsightsV2.FraudCheck
             new PhoneData("16197363066", "Orange France", "MOBILE"),
             Maybe<FraudScore>.Some(new FraudScore("54", RiskRecommendation.Flag, FraudScoreLabel.Medium, "completed")),
             Maybe<SimSwap>.Some(new SimSwap(SimSwapStatus.Failed, true, "Mobile Network Operator Not Supported")));
-
-        internal static Result<FraudCheckRequest> BuildRequestWithFraudScore() =>
-            FraudCheckRequest.Build()
-                .WithPhone("447009000000")
-                .WithFraudScore()
-                .Create();
     }
 }
