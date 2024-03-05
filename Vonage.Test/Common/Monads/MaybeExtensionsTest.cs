f6954b64703c5f66d1bfadd9e929759ee452b1a3
tr00d <guillaume.faas@live.fr>
2024-03-05 09:26:13 +0100

    refactor: move tests behaviors for Monads in a specific collection

--- Vonage.Test/Common/Monads/MaybeExtensionsTest.cs
+++ Vonage.Test/Common/Monads/MaybeExtensionsTest.cs
@@ -11,77 +11,70 @@ public class MaybeExtensionsTest
     [Fact]
     public async Task BindAsync_ShouldReturnNone_GivenValueIsNone()
     {
-        var result = await CreateNoneAsync<int>().BindAsync(IncrementBindAsync);
+        var result = await MaybeBehaviors.CreateNoneAsync<int>().BindAsync(MaybeBehaviors.IncrementBindAsync);
         result.Should().BeNone();
     }
 
     [Fact]
     public async Task BindAsync_ShouldReturnSome_GivenValueIsSome()
     {
-        var result = await CreateSomeAsync(10).BindAsync(IncrementBindAsync);
+        var result = await MaybeBehaviors.CreateSomeAsync(10).BindAsync(MaybeBehaviors.IncrementBindAsync);
         result.Should().BeSome(11);
     }
 
     [Fact]
     public async Task Bind_ShouldReturnNone_GivenValueIsNone()
     {
-        var result = await CreateNoneAsync<int>().Bind(IncrementBind);
+        var result = await MaybeBehaviors.CreateNoneAsync<int>().Bind(MaybeBehaviors.IncrementBind);
         result.Should().BeNone();
     }
 
     [Fact]
     public async Task Bind_ShouldReturnSome_GivenValueIsSome()
     {
-        var result = await CreateSomeAsync(10).Bind(IncrementBind);
+        var result = await MaybeBehaviors.CreateSomeAsync(10).Bind(MaybeBehaviors.IncrementBind);
         result.Should().BeSome(11);
     }
 
     [Fact]
     public async Task Map_ShouldReturnNone_GivenValueIsNone()
     {
-        var result = await CreateNoneAsync<int>().Map(Increment);
+        var result = await MaybeBehaviors.CreateNoneAsync<int>().Map(MaybeBehaviors.Increment);
         result.Should().BeNone();
     }
 
     [Fact]
     public async Task Map_ShouldReturnSome_GivenValueIsSome()
     {
-        var result = await CreateSomeAsync(10).Map(Increment);
+        var result = await MaybeBehaviors.CreateSomeAsync(10).Map(MaybeBehaviors.Increment);
         result.Should().BeSome(11);
     }
 
     [Fact]
     public async Task MapAsync_ShouldReturnNone_GivenValueIsNone()
     {
-        var result = await CreateNoneAsync<int>().MapAsync(IncrementAsync);
+        var result = await MaybeBehaviors.CreateNoneAsync<int>().MapAsync(MaybeBehaviors.IncrementAsync);
         result.Should().BeNone();
     }
 
     [Fact]
     public async Task MapAsync_ShouldReturnSome_GivenValueIsSome()
     {
-        var result = await CreateSomeAsync(10).MapAsync(IncrementAsync);
+        var result = await MaybeBehaviors.CreateSomeAsync(10).MapAsync(MaybeBehaviors.IncrementAsync);
         result.Should().BeSome(11);
     }
 
     [Fact]
     public async Task Match_ShouldReturnNone_GivenValueIsNone()
     {
-        var result = await CreateNoneAsync<int>().Match(_ => "some", () => "none");
+        var result = await MaybeBehaviors.CreateNoneAsync<int>().Match(_ => "some", () => "none");
         result.Should().Be("none");
     }
 
     [Fact]
     public async Task Match_ShouldReturnSome_GivenValueIsSome()
     {
-        var result = await CreateSomeAsync(10).Match(_ => "some", () => "none");
+        var result = await MaybeBehaviors.CreateSomeAsync(10).Match(_ => "some", () => "none");
         result.Should().Be("some");
     }
-
-    private static Task<Maybe<T>> CreateSomeAsync<T>(T value) => Task.FromResult(Maybe<T>.Some(value));
-    private static Task<Maybe<T>> CreateNoneAsync<T>() => Task.FromResult(Maybe<T>.None);
-    private static int Increment(int value) => value + 1;
-    private static Task<int> IncrementAsync(int value) => Task.FromResult(value + 1);
-    private static Maybe<int> IncrementBind(int value) => value + 1;
-    private static Task<Maybe<int>> IncrementBindAsync(int value) => Task.FromResult(Maybe<int>.Some(value + 1));
 }
--- Vonage.Test/Common/Monads/ResultExtensionsTest.cs
+++ Vonage.Test/Common/Monads/ResultExtensionsTest.cs
@@ -1,6 +1,5 @@
 ﻿using System.Threading.Tasks;
 using FluentAssertions;
-using Vonage.Common.Failures;
 using Vonage.Common.Monads;
 using Vonage.Test.Common.Extensions;
 using Xunit;
@@ -14,98 +13,98 @@ public class ResultExtensionsTest
     [Fact]
     public async Task BindAsync_ShouldReturnFailure_GivenValueIsFailure()
     {
-        var result = await CreateFailureAsync<int>().BindAsync(IncrementBindAsync);
-        result.Should().BeFailure(CreateFailure());
+        var result = await TestBehaviors.CreateFailureAsync<int>().BindAsync(TestBehaviors.IncrementBindAsync);
+        result.Should().BeFailure(TestBehaviors.CreateResultFailure());
     }
 
     [Fact]
     public async Task BindAsync_ShouldReturnSuccess_GivenValueIsSuccess()
     {
-        var result = await CreateSuccessAsync(10).BindAsync(IncrementBindAsync);
+        var result = await TestBehaviors.CreateSuccessAsync(10).BindAsync(TestBehaviors.IncrementBindAsync);
         result.Should().BeSuccess(11);
     }
 
     [Fact]
     public async Task Bind_ShouldReturnFailure_GivenValueIsFailure()
     {
-        var result = await CreateFailureAsync<int>().Bind(IncrementBind);
-        result.Should().BeFailure(CreateFailure());
+        var result = await TestBehaviors.CreateFailureAsync<int>().Bind(TestBehaviors.IncrementBind);
+        result.Should().BeFailure(TestBehaviors.CreateResultFailure());
     }
 
     [Fact]
     public async Task Bind_ShouldReturnSuccess_GivenValueIsSuccess()
     {
-        var result = await CreateSuccessAsync(10).Bind(IncrementBind);
+        var result = await TestBehaviors.CreateSuccessAsync(10).Bind(TestBehaviors.IncrementBind);
         result.Should().BeSuccess(11);
     }
 
     [Fact]
     public async Task Map_ShouldReturnFailure_GivenValueIsFailure()
     {
-        var result = await CreateFailureAsync<int>().Map(Increment);
-        result.Should().BeFailure(CreateFailure());
+        var result = await TestBehaviors.CreateFailureAsync<int>().Map(TestBehaviors.Increment);
+        result.Should().BeFailure(TestBehaviors.CreateResultFailure());
     }
 
     [Fact]
     public async Task Map_ShouldReturnSuccess_GivenValueIsSuccess()
     {
-        var result = await CreateSuccessAsync(10).Map(Increment);
+        var result = await TestBehaviors.CreateSuccessAsync(10).Map(TestBehaviors.Increment);
         result.Should().BeSuccess(11);
     }
 
     [Fact]
     public async Task MapAsync_ShouldReturnFailure_GivenValueIsFailure()
     {
-        var result = await CreateFailureAsync<int>().MapAsync(IncrementAsync);
-        result.Should().BeFailure(CreateFailure());
+        var result = await TestBehaviors.CreateFailureAsync<int>().MapAsync(TestBehaviors.IncrementAsync);
+        result.Should().BeFailure(TestBehaviors.CreateResultFailure());
     }
 
     [Fact]
     public async Task MapAsync_ShouldReturnSuccess_GivenValueIsSuccess()
     {
-        var result = await CreateSuccessAsync(10).MapAsync(IncrementAsync);
+        var result = await TestBehaviors.CreateSuccessAsync(10).MapAsync(TestBehaviors.IncrementAsync);
         result.Should().BeSuccess(11);
     }
 
     [Fact]
     public async Task IfSuccessAsync_ShouldExecuteOperation_GivenSuccess()
     {
-        await CreateSuccessAsync(10).IfSuccessAsync(this.SuccessAction);
+        await TestBehaviors.CreateSuccessAsync(10).IfSuccessAsync(this.SuccessAction);
         this.value.Should().Be(10);
     }
 
     [Fact]
     public async Task IfSuccessAsync_ShouldReturnSuccess_GivenSuccess()
     {
-        var result = await CreateSuccessAsync(10).IfSuccessAsync(this.SuccessAction);
+        var result = await TestBehaviors.CreateSuccessAsync(10).IfSuccessAsync(this.SuccessAction);
         result.Should().BeSuccess(10);
     }
 
     [Fact]
     public async Task IfSuccessAsync_ShouldNotExecuteOperation_GivenFailure()
     {
-        await CreateFailureAsync<int>().IfSuccessAsync(this.SuccessAction);
+        await TestBehaviors.CreateFailureAsync<int>().IfSuccessAsync(this.SuccessAction);
         this.value.Should().Be(default);
     }
 
     [Fact]
     public async Task IfSuccessAsync_ShouldReturnFailure_GivenFailure()
     {
-        var result = await CreateFailureAsync<int>().IfSuccessAsync(this.SuccessAction);
-        result.Should().BeFailure(CreateFailure());
+        var result = await TestBehaviors.CreateFailureAsync<int>().IfSuccessAsync(this.SuccessAction);
+        result.Should().BeFailure(TestBehaviors.CreateResultFailure());
     }
 
     [Fact]
     public async Task IfFailure_ShouldReturnFallbackValue_GivenFailure()
     {
-        var result = await CreateFailureAsync<int>().IfFailure(50);
+        var result = await TestBehaviors.CreateFailureAsync<int>().IfFailure(50);
         result.Should().Be(50);
     }
 
     [Fact]
     public async Task IfFailure_ShouldReturnSuccess_GivenSuccess()
     {
-        var result = await CreateSuccessAsync(10).IfFailure(50);
+        var result = await TestBehaviors.CreateSuccessAsync(10).IfFailure(50);
         result.Should().Be(10);
     }
 
@@ -114,14 +113,4 @@ public class ResultExtensionsTest
         this.value = input;
         return Task.CompletedTask;
     }
-
-    private static Task<Result<T>> CreateSuccessAsync<T>(T value) => Task.FromResult(Result<T>.FromSuccess(value));
-    private static Task<Result<T>> CreateFailureAsync<T>() => Task.FromResult(Result<T>.FromFailure(CreateFailure()));
-    private static ResultFailure CreateFailure() => ResultFailure.FromErrorMessage("Error");
-    private static int Increment(int value) => value + 1;
-    private static Task<int> IncrementAsync(int value) => Task.FromResult(value + 1);
-    private static Result<int> IncrementBind(int value) => value + 1;
-
-    private static Task<Result<int>> IncrementBindAsync(int value) =>
-        Task.FromResult(Result<int>.FromSuccess(value + 1));
 }
