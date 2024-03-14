a832a3d0d2774a18c154e7f3162781f62179881f
tr00d <guillaume.faas@live.fr>
2024-03-14 15:51:46 +0100

    refactor: [breaking] remove obsolete VoiceName from TalkCommand

--- Vonage/Voice/TalkCommand.cs
+++ Vonage/Voice/TalkCommand.cs
@@ -1,5 +1,4 @@
 ﻿using Newtonsoft.Json;
-using System;
 
 namespace Vonage.Voice;
 
@@ -14,22 +13,12 @@ public class TalkCommand
     public string Text { get; set; }
 
     /// <summary>
-    /// The name of the voice used to deliver text. You use the voice_name
-    /// that has the correct language, gender and accent for the message
-    /// you are sending. For example, the default voice kimberley is a
-    /// female who speaks English with an American accent (en-US).
-    /// Possible values for voice_name are listed at https://docs.nexmo.com/voice/voice-api/api-reference#talk_put
-    /// </summary>
-    [JsonProperty("voice_name")]
-    [Obsolete("This parameter has been made obsolete by the language and style fields. Please see https://developer.nexmo.com/voice/voice-api/guides/text-to-speech#locale for more details")]
-    public string VoiceName { get; set; }
-
-    /// <summary>
     /// Set to 0 to replay the audio file at stream_url when the stream ends. The default value is 1.
     /// </summary>
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Include, NullValueHandling = NullValueHandling.Ignore, PropertyName ="loop")]
+    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Include, NullValueHandling = NullValueHandling.Ignore,
+        PropertyName = "loop")]
     public int? Loop { get; set; }
-        
+
     /// <summary>
     /// The volume level that the speech is played. This can be any value between -1 to 1 in 0.1 increments, with 0 being the default.
     /// </summary>
