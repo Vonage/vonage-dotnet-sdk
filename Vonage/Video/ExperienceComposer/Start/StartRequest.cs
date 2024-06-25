5b9131886f65521eb2fe667b9e5f32170ec80f21
tr00d <guillaume.faas@live.fr>
2024-06-25 08:45:47 +0200

    feat: implement serialization for Start on ExperienceComposer

--- Vonage/Video/ExperienceComposer/Start/StartRequest.cs
+++ Vonage/Video/ExperienceComposer/Start/StartRequest.cs
@@ -1,43 +1,60 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text;
+using System.Text.Json.Serialization;
 using Vonage.Common.Client;
+using Vonage.Serialization;
 using Vonage.Server;
 
 namespace Vonage.Video.ExperienceComposer.Start;
 
 /// <inheritdoc />
-public struct StartRequest : IVonageRequest
+public readonly struct StartRequest : IVonageRequest
 {
     /// <summary>
-    /// </summary>
+    [JsonIgnore]
     public Guid ApplicationId { get; internal init; }
 
     /// <summary>
     /// </summary>
+    ///
+    [JsonPropertyOrder(0)]
     public string SessionId { get; internal init; }
 
     /// <summary>
     /// </summary>
+    [JsonPropertyOrder(1)]
     public string Token { get; internal init; }
 
     /// <summary>
     /// </summary>
+    [JsonPropertyOrder(2)]
     public Uri Url { get; internal init; }
 
     /// <summary>
     /// </summary>
+    [JsonPropertyOrder(3)]
     public int MaxDuration { get; internal init; }
 
     /// <summary>
     /// </summary>
+    [JsonPropertyOrder(4)]
     public RenderResolution Resolution { get; internal init; }
 
     /// <summary>
     /// </summary>
+    [JsonPropertyOrder(5)]
     public StartProperties Properties { get; internal init; }
 
     /// <inheritdoc />
-    public HttpRequestMessage BuildRequestMessage() => throw new NotImplementedException();
+    public HttpRequestMessage BuildRequestMessage() => VonageRequestBuilder
+        .Initialize(HttpMethod.Post, this.GetEndpointPath())
+        .WithContent(this.GetRequestContent())
+        .Build();
+
+    private StringContent GetRequestContent() =>
+        new StringContent(JsonSerializerBuilder.BuildWithCamelCase().SerializeObject(this), Encoding.UTF8,
+            "application/json");
 
     /// <inheritdoc />
     public string GetEndpointPath() => $"/v2/project/{this.ApplicationId}/render";
