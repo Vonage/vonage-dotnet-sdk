66b32aaebb417cff719a65cc38b3d2227bc5a051
Patrik Westerlund <patrik.wlund@gmail.com>
2025-02-07 10:34:35 +0100

    Inline color translation library

---
+++ Vonage/Common/Serialization/ColorTranslator/ARGB.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Vonage.Common.Serialization.ColorTranslator;
+
+// From: https://github.com/stephenhand/.NETCore-Compatibility-Helpers/blob/master/src/ColorTranslations/ARGB.cs
+// Credit: Stephen Hand
+
+internal struct ARGB
+{
+    private readonly int a, r, g, b;
+
+    public ARGB(int a, int r, int g, int b)
+    {
+        this.a = a;
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+    public ARGB(int r, int g, int b) : this(255, r, g, b) { }
+
+    public int A { get { return a; } }
+    public int R { get { return r; } }
+    public int G { get { return g; } }
+    public int B { get { return b; } }
+
+    public override string ToString()
+    {
+        return string.Format("A:{0}, R:{1}, G:{2}, B:{3}");
+    }
+}
