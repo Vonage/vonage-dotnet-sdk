cf54d92c2db9a2f2b346413b21e6f94456eac728
tr00d <guillaume.faas@live.fr>
2025-09-08 06:51:23 +0200

    refactor: move EndpointTest under Voice

---
+++ Vonage.Test/Voice/EndpointTest.cs
@@ -0,0 +1,49 @@
+﻿#region
+using Newtonsoft.Json;
+using Vonage.Serialization;
+using Vonage.Voice;
+using Vonage.Voice.Nccos.Endpoints;
+using Xunit;
+#endregion
+
+namespace Vonage.Test.Voice;
+
+[Trait("Category", "Legacy")]
+public class EndpointTest
+{
+    [Fact]
+    public void TestNccoEndpoint()
+    {
+        var expected =
+            "{\"uri\":\"wss://www.example.com/ws\",\"content-type\":\"audio/l16;rate=16000\",\"headers\":{\"Bar\":\"bar\"},\"type\":\"websocket\"}";
+        var websocketEndpoint = new WebsocketEndpoint
+        {
+            Uri = "wss://www.example.com/ws",
+            Headers = new Foo {Bar = "bar"},
+            ContentType = "audio/l16;rate=16000",
+        };
+        var json = JsonConvert.SerializeObject(websocketEndpoint, VonageSerialization.SerializerSettings);
+        Assert.Equal(expected, json);
+    }
+
+    [Fact]
+    public void TestWebhookEndpoint()
+    {
+        var expected =
+            "{\"type\":\"websocket\",\"uri\":\"wss://www.example.com/ws\",\"content-type\":\"audio/l16;rate=16000\",\"headers\":{\"Bar\":\"bar\"}}";
+        var websocketEndpoint = new CallEndpoint
+        {
+            Type = "websocket",
+            Uri = "wss://www.example.com/ws",
+            Headers = new Foo {Bar = "bar"},
+            ContentType = "audio/l16;rate=16000",
+        };
+        var json = JsonConvert.SerializeObject(websocketEndpoint, VonageSerialization.SerializerSettings);
+        Assert.Equal(expected, json);
+    }
+
+    public class Foo
+    {
+        public string Bar { get; set; }
+    }
+}
