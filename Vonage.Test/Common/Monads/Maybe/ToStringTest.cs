a93fc5d3bef98cf976d41c8efe3ac7b8ba0baa21
tr00d <guillaume.faas@live.fr>
2025-12-18 08:38:17 +0100

    refactor: unshelf ToString tests for Maybe in specific test file

---
+++ Vonage.Test/Common/Monads/Maybe/ToStringTest.cs
@@ -0,0 +1,17 @@
+﻿#region
+using FluentAssertions;
+using Xunit;
+#endregion
+
+namespace Vonage.Test.Common.Monads.Maybe;
+
+public class ToStringTest
+{
+    [Fact]
+    public void ToString_ShouldReturnNone_GivenValueIsNone() =>
+        MaybeBehaviors.CreateNone<int>().ToString().Should().Be("None");
+
+    [Fact]
+    public void ToString_ShouldReturnSome_GivenValueIsSome() => MaybeBehaviors.CreateSome(10).ToString().Should()
+        .Be("Some(Vonage.Common.Monads.Maybe`1[System.Int32])");
+}
