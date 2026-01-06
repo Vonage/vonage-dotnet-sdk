b3d8e12eb4ef77c6bde889f2be70b84b792fa948
tr00d <guillaume.faas@live.fr>
2026-01-06 11:25:14 +0100

    refactor: remove dependency from TestBase on ApplicationTests

--- Vonage.Test/Applications/ApplicationTests.cs
+++ Vonage.Test/Applications/ApplicationTests.cs
@@ -1,36 +1,67 @@
 ﻿#region
+using System;
+using System.Net;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Vonage.Applications;
-using Vonage.Common.Monads;
-using Vonage.Request;
 using Vonage.Serialization;
 using Vonage.Test.Common;
+using Vonage.Test.TestHelpers;
+using WireMock.ResponseBuilders;
 using Xunit;
 #endregion
 
 namespace Vonage.Test.Applications;
 
 [Trait("Category", "Legacy")]
-public class ApplicationTests : TestBase
+public class ApplicationTests : IDisposable
 {
+    private readonly TestingContext context = TestingContext.WithBasicCredentials();
+
     private readonly SerializationTestHelper helper = new SerializationTestHelper(typeof(ApplicationTests).Namespace,
         JsonSerializerBuilder.BuildWithCamelCase());
 
-    private Credentials Credentials => Credentials.FromApiKeyAndSecret(this.ApiKey, this.ApiSecret);
+    public void Dispose()
+    {
+        this.context?.Dispose();
+        GC.SuppressFinalize(this);
+    }
+
+    private IResponseBuilder RespondWithSuccess([CallerMemberName] string testName = null) =>
+        Response.Create()
+            .WithStatusCode(HttpStatusCode.OK)
+            .WithBody(this.helper.GetResponseJson(testName));
+
+    private IResponseBuilder RespondWithRequestJson([CallerMemberName] string testName = null) =>
+        Response.Create()
+            .WithStatusCode(HttpStatusCode.OK)
+            .WithBody(this.helper.GetResponseJson(testName));
 
     [Fact]
     public async Task CreateApplication()
     {
-        this.Setup($"{this.ApiUrl}/v2/applications", this.helper.GetResponseJson(), this.helper.GetRequestJson());
-        var response = await this.BuildApplicationClient().CreateApplicationAsync(ApplicationTestData.CreateRequest());
+        this.context.Server.Given(WireMock.RequestBuilders.Request.Create()
+                .WithPath("/v2/applications")
+                .WithHeader("Authorization", this.context.ExpectedAuthorizationHeaderValue)
+                .WithBodyAsJson(this.helper.GetRequestJson())
+                .UsingPost())
+            .RespondWith(this.RespondWithRequestJson());
+        var response =
+            await this.context.VonageClient.ApplicationClient.CreateApplicationAsync(
+                ApplicationTestData.CreateRequest());
         response.ShouldMatchExpectedApplication();
     }
 
     [Fact]
     public async Task CreateVideoApplication()
     {
-        this.Setup($"{this.ApiUrl}/v2/applications", this.helper.GetResponseJson(), this.helper.GetRequestJson());
-        var response = await this.BuildApplicationClient()
+        this.context.Server.Given(WireMock.RequestBuilders.Request.Create()
+                .WithPath("/v2/applications")
+                .WithHeader("Authorization", this.context.ExpectedAuthorizationHeaderValue)
+                .WithBodyAsJson(this.helper.GetRequestJson())
+                .UsingPost())
+            .RespondWith(this.RespondWithRequestJson());
+        var response = await this.context.VonageClient.ApplicationClient
             .CreateApplicationAsync(ApplicationTestData.CreateVideoRequest());
         response.ShouldMatchVideoApplication();
     }
@@ -38,8 +69,13 @@ public class ApplicationTests : TestBase
     [Fact]
     public async Task CreateVideoFullApplication()
     {
-        this.Setup($"{this.ApiUrl}/v2/applications", this.helper.GetResponseJson(), this.helper.GetRequestJson());
-        var response = await this.BuildApplicationClient()
+        this.context.Server.Given(WireMock.RequestBuilders.Request.Create()
+                .WithPath("/v2/applications")
+                .WithHeader("Authorization", this.context.ExpectedAuthorizationHeaderValue)
+                .WithBodyAsJson(this.helper.GetRequestJson())
+                .UsingPost())
+            .RespondWith(this.RespondWithRequestJson());
+        var response = await this.context.VonageClient.ApplicationClient
             .CreateApplicationAsync(ApplicationTestData.CreateVideoFullRequest());
         response.ShouldMatchVideoFullApplication();
     }
@@ -47,33 +83,58 @@ public class ApplicationTests : TestBase
     [Fact]
     public async Task DeleteApplication()
     {
-        this.Setup($"{this.ApiUrl}/v2/applications/78d335fa323d01149c3dd6f0d48968cf", Maybe<string>.Some(""));
-        var result = await this.BuildApplicationClient().DeleteApplicationAsync("78d335fa323d01149c3dd6f0d48968cf");
+        this.context.Server.Given(WireMock.RequestBuilders.Request.Create()
+                .WithPath("/v2/applications/78d335fa323d01149c3dd6f0d48968cf")
+                .WithHeader("Authorization", this.context.ExpectedAuthorizationHeaderValue)
+                .UsingDelete())
+            .RespondWith(Response.Create()
+                .WithStatusCode(HttpStatusCode.NoContent)
+                .WithBody(""));
+        var result =
+            await this.context.VonageClient.ApplicationClient.DeleteApplicationAsync(
+                "78d335fa323d01149c3dd6f0d48968cf");
         Assert.True(result);
     }
 
     [Fact]
     public async Task GetApplication()
     {
-        this.Setup($"{this.ApiUrl}/v2/applications/78d335fa323d01149c3dd6f0d48968cf", this.helper.GetResponseJson());
-        var application = await this.BuildApplicationClient().GetApplicationAsync("78d335fa323d01149c3dd6f0d48968cf");
+        this.context.Server.Given(WireMock.RequestBuilders.Request.Create()
+                .WithPath("/v2/applications/78d335fa323d01149c3dd6f0d48968cf")
+                .WithHeader("Authorization", this.context.ExpectedAuthorizationHeaderValue)
+                .UsingGet())
+            .RespondWith(this.RespondWithSuccess());
+        var application =
+            await this.context.VonageClient.ApplicationClient.GetApplicationAsync("78d335fa323d01149c3dd6f0d48968cf");
         application.ShouldMatchExpectedApplication();
     }
 
     [Fact]
     public async Task ListApplications()
     {
-        this.Setup($"{this.ApiUrl}/v2/applications", this.helper.GetResponseJson());
-        var applications = await this.BuildApplicationClient().ListApplicationsAsync(new ListApplicationsRequest());
+        this.context.Server.Given(WireMock.RequestBuilders.Request.Create()
+                .WithPath("/v2/applications")
+                .WithHeader("Authorization", this.context.ExpectedAuthorizationHeaderValue)
+                .UsingGet())
+            .RespondWith(this.RespondWithSuccess());
+        var applications =
+            await this.context.VonageClient.ApplicationClient.ListApplicationsAsync(new ListApplicationsRequest());
         applications.ShouldHaveExpectedApplicationInList();
     }
 
     [Fact]
     public async Task ListApplications_WithPagination()
     {
-        this.Setup($"{this.ApiUrl}/v2/applications?page_size=10&page=1&",
-            this.helper.GetResponseJson(nameof(this.ListApplications)));
-        var applications = await this.BuildApplicationClient()
+        this.context.Server.Given(WireMock.RequestBuilders.Request.Create()
+                .WithPath("/v2/applications")
+                .WithParam("page_size", "10")
+                .WithParam("page", "1")
+                .WithHeader("Authorization", this.context.ExpectedAuthorizationHeaderValue)
+                .UsingGet())
+            .RespondWith(Response.Create()
+                .WithStatusCode(HttpStatusCode.OK)
+                .WithBody(this.helper.GetResponseJson(nameof(this.ListApplications))));
+        var applications = await this.context.VonageClient.ApplicationClient
             .ListApplicationsAsync(new ListApplicationsRequest {Page = 1, PageSize = 10});
         applications.ShouldHaveExpectedApplicationInList();
     }
@@ -81,12 +142,15 @@ public class ApplicationTests : TestBase
     [Fact]
     public async Task UpdateApplication()
     {
-        this.Setup($"{this.ApiUrl}/v2/applications/78d335fa323d01149c3dd6f0d48968cf", this.helper.GetResponseJson(),
-            this.helper.GetRequestJson());
-        var response = await this.BuildApplicationClient().UpdateApplicationAsync("78d335fa323d01149c3dd6f0d48968cf",
+        this.context.Server.Given(WireMock.RequestBuilders.Request.Create()
+                .WithPath("/v2/applications/78d335fa323d01149c3dd6f0d48968cf")
+                .WithHeader("Authorization", this.context.ExpectedAuthorizationHeaderValue)
+                .WithBodyAsJson(this.helper.GetRequestJson())
+                .UsingPut())
+            .RespondWith(this.RespondWithRequestJson());
+        var response = await this.context.VonageClient.ApplicationClient.UpdateApplicationAsync(
+            "78d335fa323d01149c3dd6f0d48968cf",
             ApplicationTestData.CreateRequest());
         response.ShouldMatchExpectedApplication();
     }
-
-    private IApplicationClient BuildApplicationClient() => this.BuildVonageClient(this.Credentials).ApplicationClient;
 }
