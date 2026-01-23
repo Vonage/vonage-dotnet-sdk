9475c17f4d2d69a8d81d1881431863f91f51a02e
tr00d <guillaume.faas@live.fr>
2026-01-23 13:46:47 +0100

    refactor: transform result state when evaluating parsing rules

--- Vonage/Common/Validation/InputEvaluation.cs
+++ Vonage/Common/Validation/InputEvaluation.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿#region
+using System;
 using System.Linq;
 using Vonage.Common.Failures;
 using Vonage.Common.Monads;
+#endregion
 
 namespace Vonage.Common.Validation;
 
@@ -9,10 +11,16 @@ internal readonly struct InputEvaluation<T>
 {
     private readonly T source;
     private InputEvaluation(T source) => this.source = source;
-    
+
     public static InputEvaluation<T> Evaluate(T source) => new InputEvaluation<T>(source);
-    
+
     public Result<T> WithRules(params Func<T, Result<T>>[] parsingRules)
+    {
+        var result = parsingRules.Aggregate(Result<T>.FromSuccess(this.source), (result, rule) => result.Bind(rule));
+        return result.IsSuccess ? result : this.ListFailures(parsingRules);
+    }
+
+    private Result<T> ListFailures(Func<T, Result<T>>[] parsingRules)
     {
         var copy = this.source;
         var failures = parsingRules
@@ -21,8 +29,6 @@ internal readonly struct InputEvaluation<T>
             .Where(failure => !string.IsNullOrEmpty(failure))
             .Select(ResultFailure.FromErrorMessage)
             .ToArray();
-        return failures.Length == 0
-            ? Result<T>.FromSuccess(this.source)
-            : Result<T>.FromFailure(ParsingFailure.FromFailures(failures));
+        return Result<T>.FromFailure(ParsingFailure.FromFailures(failures));
     }
 }
